92198b5c49bf891a208757f8579442e1914735b9
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-09 22:19:28 -0300

    Context criado e incorporado

---
+++ front-end/src/context/index.js
@@ -0,0 +1,29 @@
+import React, { createContext, useState } from 'react';
+
+// Isso aqui cria o Contexto da nossa aplicação.
+// Chamei de Context, mas você pode dar o nome que quiser.
+const Context = createContext();
+
+// Esse é o fornecedor do contexto da aplicação
+const TrybeerContextProvider = ({ children }) => {
+  // useState(false) isso define um estado booleano
+  // o useState nos retorna um array com uma variável e uma função, respectivamente e nessa ordem.
+  // Você define o que será seu estado nessa variável
+  // Eu defini que fosse um booleano, mas pode ser um array, um objeto, um objeto de objetos...
+  // um array de objetos, ou a estrutura de dados que você precisar e achar melhor pra trabalhar.
+  const [meuEstado, setMeuEstado] = useState(false);
+  const contextValue = {
+    meuEstado, // uma variável de estado
+    setMeuEstado, // essa é a função que muda a variável de estado
+  };
+  // No contexto você sempre retorna o Context criado usando a função createContext();
+  // Daí você chama o provider desse contexto
+  // e como valor você passa um objeto com suas variáveis de estado e funções que alteram elas
+  // Children é o elemento que será envolvido com esse Context, no caso, nossa aplicação toda.
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
+};
+
+// Exporte o Context, pois você precisa dele quando for usar suas variáveis e funções
+// Exporte o seu Provider, ele é quem vai encapsular a aplicação toda.
+// Vou colocar o Provider no arquivo de routes.js
+export { Context, TrybeerContextProvider as Provider };
--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
+import { Provider } from './context';
+
 const Routes = () => (
-  <Switch>
-    Hello World!
-    {/* <Route path="" component={ '' } /> */}
-  </Switch>
+  <Provider>
+    <Switch>
+      Hello World!
+      {/* <Route path="" component={ '' } /> */}
+    </Switch>
+  </Provider>
 );
 
 export default Routes;
