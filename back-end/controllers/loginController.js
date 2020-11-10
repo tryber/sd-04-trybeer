467f327849a530ac724881b5f64597fa5ad68604
Victor L <victor.lustosacp@gmail.com>
2020-11-10 14:23:19 -0300

    cc

--- back-end/controllers/loginController.js
+++ back-end/controllers/loginController.js
@@ -2,7 +2,7 @@ const loginService = require('../services/loginService');
 
 const userLogin = async (_req, _res) => {
   const login = await loginService.userLogin('tryber@trybe.com.br', 123456);
-  console.log(login);
+  resizeBy.status(200).json(login);
 };
 
 module.exports = { userLogin };
--- back-end/middleware/validations.js
+++ back-end/middleware/validations.js
@@ -1,11 +1,7 @@
 const joi = require('joi');
 
 const loginSchema = joi.object({
-  email: joi
-    .string()
-    .email({ tlds: { allow: false } })
-    .required(),
-
+  email: joi.string().email({ tlds: { allow: false } }).required(),
   password: joi.string().min(6).required(),
 });
 
