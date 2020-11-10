0349d055fc8d5fd5e7a70e678a38f942381966f9
frederico-campello <fredfmc33@gmail.com>
2020-11-10 17:59:24 -0300

    continuando req 01

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,20 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [users, setUsers] = useState(null);
+
+  const [admin, setAdmin] = useState(false);
+  const [client, setClient] = useState(false);
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/users")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   const validEmail = (email) =>
     /[A-Z0-9]{1,}@[A-Z0-9]{2,}\.[A-Z0-9]{2,}/i.test(email);
 
-  // localStorage.setItem(user, object)
+  const createToken = () =>
+    axios
+      .post("http://localhost:3001/login", { email, password })
+      .then((res) => setToken(res.data.token))
+      .catch((error) => console.log(error));
 
-  // req.body = {email: '', password: ''}
+  // if (admin) return <Redirect to="/admin/orders" />;
+  // if (client) return <Redirect to="/products" />;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = users.find(
+      (item) => item.email === email && item.password === password
+    );
+
+    const { name, role } = user;
+
+    createToken();
+
+    if (token !== "") {
+      const objUser = { name, email, token, role };
+      localStorage.setItem("user", JSON.stringify(objUser));
+    }
+
+    role === "administrator" ? setAdmin(true) : setClient(true);
+  };
 
   return (
     <div>
       <label htmlFor="email">Email</label>
-      <form action="/" method="POST">
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input
           type="email"
           name="email"
@@ -38,9 +80,11 @@ const Login = () => {
           ENTRAR
         </button>
       </form>
-      <button type="button" data-testid="no-account-btn">
-        Ainda não tenho conta
-      </button>
+      <Link to="/register">
+        <button type="button" data-testid="no-account-btn">
+          Ainda não tenho conta
+        </button>
+      </Link>
     </div>
   );
 };
