200e0806ec91e1c9106968e99afa39771cdb0818
Pedro <pedrotpoliveira@gmail.com>
2020-11-11 17:21:18 -0300

    Refactoring - suggestions from @danielPantalena

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import {
-  Checkout, Home, Login, NotFound, Orders, OrdersDetails, Products, Profile, Register,
-} from './pages';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Switch>
-        <Home exact path="/" />
-        <Checkout path="/checkout" />
-        <Login path="/login" />
-        <Orders exact path="/orders" />
-        <Route path="/orders/:id">
-          <OrdersDetails />
-        </Route>
-        <Orders exact path="/admin/orders" />
-        <Route path="/admin/orders/:id">
-          <OrdersDetails />
-        </Route>
-        <Products path="/products" />
-        <Profile path="/profile" />
-        <Profile path="/admin/profile" />
-        <Register path="/register" />
-        <Route component={ NotFound } />
-      </Switch>
+      <Routes />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
