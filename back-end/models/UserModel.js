0242d03e7660474275cd1b1fce2404d821915871
henrique <hbaetaleite@gmail.com>
2020-11-11 17:25:22 -0300

    codeclimate v4

--- back-end/models/UserModel.js
+++ back-end/models/UserModel.js
@@ -3,10 +3,10 @@ const connection = require('./connection');
 const registerUser = async (name, email, password, role) => {
   const result = await connection()
     .then((db) => db
-        .getTable('users')
-        .insert(['name', 'email', 'password', 'role'])
-        .values([name, email, password, role])
-        .execute())
+      .getTable('users')
+      .insert(['name', 'email', 'password', 'role'])
+      .values([name, email, password, role])
+      .execute())
     .catch((err) => {
       throw err;
     });
@@ -14,15 +14,14 @@ const registerUser = async (name, email, password, role) => {
 };
 
 const findUserByEmail = async (emailInput) => {
-  const result =await connection()
+  const result = await connection()
     .then((db) => db
-        .getTable('users')
-        .select([])
-        .where('email = :email')
-        .bind('email', emailInput)
-        .then((results) => results.fetchOne())
-        .then(([id, name, email, password, role]) => ({ id, name, email, password, role })),
-    )
+      .getTable('users')
+      .select([])
+      .where('email = :email')
+      .bind('email', emailInput)
+      .then((results) => results.fetchOne())
+      .then(([id, name, email, password, role]) => ({ id, name, email, password, role }))    )
     .catch((err) => {
       throw err;
     });
