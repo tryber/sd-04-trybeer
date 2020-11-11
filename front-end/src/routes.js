bc18e83a064f640e9b9f8b84d484ae49ee3aed66
Giorgia <giorgialeoniac@gmail.com>
2020-11-11 17:25:25 -0300

    Before pull

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import { Provider } from './context';
-import Registro from './pages/Registro'
+import Registro from './pages/Registro';
+import ClientProfile from './pages/ClientProfile/ClientProfile';
 
 const Routes = () => (
   <Provider>
-    <Router>
-      <Switch>
-        <Route exact path="/register" component={Registro} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/profile" component={ClientProfile} />
+      <Route exact path="/register" component={Registro} />
+    </Switch>
   </Provider>
 );
 
