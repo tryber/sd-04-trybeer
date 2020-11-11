c93a9cb7a22d6bc193ddd39aedef08e2758b541b
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-11-11 19:12:01 -0300

    Css Done

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { validateLogin } from '../services/validate';
 import { postLogin } from '../services/TrybeerApi';
+import { DuffLogoTransparent } from '../images';
+import '../css/loginPage.css';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-  const [canLogin, setCanLogin] = useState(false);
   const history = useHistory();
+  const [ message, setMessage ] = useState('');
+  const [ canLogin, setCanLogin ] = useState(false);
+  const [ form, setForm ] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    { email: '', password: '' },
+  );
+  const { email, password } = form;
+
+  const handleInput = ({ target: { name, value } }) => setForm({ [ name ]: value });
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -22,46 +30,51 @@ function Login() {
     }
   };
 
-  const validate = (userEmail, userPassword) => {
-    const { error } = validateLogin(userEmail, userPassword);
-    if (error) return setMessage(error.message);
+  useEffect(() => {
+    const { error } = validateLogin(form.email, form.password);
+    if (error) {
+      setCanLogin(false);
+      return setMessage(error.message)
+    };
+    setMessage();
     return setCanLogin(true);
-  };
+  }, [ form ])
 
   return (
-    <form onSubmit={ handleSubmit }>
-      <label htmlFor="email">
-        Email:
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-header">
+          <img src={DuffLogoTransparent} alt="Duff Logo" className="duff-logo" />
+        </div>
+        <form onSubmit={handleSubmit} className="login-form">
+          <label htmlFor="email">
+            Email:
         <input
-          type="text"
-          name="email"
-          value={ email }
-          onChange={ ({ target: { value } }) => {
-            setEmail(value);
-            validate(value, password);
-          } }
-          data-testid="email-input"
-          required
-        />
-      </label>
-      <label htmlFor="password">
-        Password:
+              type="text"
+              name="email"
+              value={email}
+              onChange={handleInput}
+              data-testid="email-input"
+              required
+            />
+          </label>
+          <label htmlFor="password">
+            Password:
         <input
-          type="password"
-          name="password"
-          value={ password }
-          onChange={ ({ target: { value } }) => {
-            setPassword(value);
-            validate(email, value);
-          } }
-          data-testid="password-input"
-          required
-        />
-      </label>
-      <button type="submit" data-testid="signin-btn" disabled={ !canLogin }>ENTRAR</button>
-      <Link to="/register" data-testid="no-account-btn">Ainda não tenho conta</Link>
-      <span>{message}</span>
-    </form>
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleInput}
+              data-testid="password-input"
+              required
+            />
+          </label>
+          <button type="submit" data-testid="signin-btn" disabled={!canLogin}>ENTRAR</button>
+          <Link to="/register" data-testid="no-account-btn">Ainda não tenho conta</Link>
+          <span>{message}</span>
+        </form>
+      </div>
+    </div>
   );
 }
 
