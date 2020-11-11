7f8f8f01138173f149f26d3e9adcba67be3ead73
Josias Viveiro <josiasviveiro@gmail.com>
2020-11-11 18:50:46 -0300

    Alteração de singlequote para quote

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -13,20 +13,20 @@ const Register = () => {
     <div>
       <FormControl>
         <FormLabel htmlFor="name">Nome</FormLabel>
-        <Input type='text' name='signName' id='name' data-testid="signup-name" />
+        <Input type="text" name="signName" id="name" data-testid="signup-name" />
 
         <FormLabel htmlFor="email">Email</FormLabel>
-        <Input type='email' name='signEmail' id="email" data-testid="signup-email" />
+        <Input type="email" name="signEmail" id="email" data-testid="signup-email" />
 
         <FormLabel htmlFor="password">Senha</FormLabel>
-        <Input type='password' name='signPassword' id="password" data-testid="signup-password" />
+        <Input type="password" name="signPassword" id="password" data-testid="signup-password" />
 
         <Checkbox variantColor="green" data-testid="signup-seller">Quero vender</Checkbox>
 
-        <Button variantColor="green" type='submit' data-testid="signup-btn">Cadastrar</Button>
+        <Button variantColor="green" type="submit" data-testid="signup-btn">Cadastrar</Button>
       </FormControl>
     </div>
-  )
+  );
 };
 
 export default Register;
