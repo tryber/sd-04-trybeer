7c241cfa4eab6753a32873532287ad16372f40ed
henrique <hbaetaleite@gmail.com>
2020-11-11 15:03:12 -0300

    add connection

---
+++ back-end/models/connection.js
@@ -0,0 +1,23 @@
+require('dotenv/config');
+const mysqlx = require('@mysql/xdevapi');
+
+const config = {
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
+  port: 33060,
+  socketPath: '/var/run/mysqld/mysqld.sock',
+};
+let schema;
+
+module.exports = async () => {
+  try {
+    if (schema) return Promise.resolve(schema);
+
+    const session = await mysqlx.getSession(config);
+
+    return (schema = session.getSchema(process.env.DB_NAME));
+  } catch (_) {
+    process.exit(1);
+  }
+};
