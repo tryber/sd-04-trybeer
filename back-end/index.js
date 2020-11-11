f25c7032bc426f926c9eb5fbe8454b1596dc1d2c
henrique <hbaetaleite@gmail.com>
2020-11-11 20:51:34 -0300

    add cors ao index backend / refatorando api do frontend

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,16 @@
 require('dotenv/config');
 const express = require('express');
 
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT;
 
 const router = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.get('/', (_, res) => res.send('ok'));
 
--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const loginApi = async (email, password) =>
-  axios.post('http://localhost:3001/login', { email, password });
+const apiTrybeer = axios.create({
+  baseURL: 'http://localhost:3001',
+});
+
+const loginApi = async (email, password) => apiTrybeer.post('/login', { email, password });
 
 const getUserByEmail = async (email) => axios.get(`/${email}`);
 
