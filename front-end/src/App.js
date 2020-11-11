04512fe7abbc7845417b5b3c7240bbc96cb94f5a
wistyk <leonardoxw@gmail.com>
2020-11-11 19:52:29 -0300

    rota criada

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Login from './components/Login';
 import UserRegister from './components/UserRegister';
+import UserProfile from './components/UserProfile/index';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/login" component={ Login } />
       <Route path="/register" component={ UserRegister } />
+      <Route path="/profile" component= { UserProfile } />
     </Switch>
   </BrowserRouter>
 );
---
+++ front-end/src/components/UserProfile/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/index';
+
+import './style.css';
+
+const UserProfile = () => {
+  return (
+    <div>
+      <Header title={'Meu Perfil'} />
+    </div>
+  );
+};
+
+export default UserProfile;
