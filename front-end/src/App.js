4e5c206a6ee68da81aab0a7f464482993c628c1f
Joao Soutto <joaosoutto@hotmail.com>
2020-11-11 13:07:22 -0300

    subindo o menu

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Home from './pages/Home';
 import Login from './pages/Login';
+import Produtos from './pages/Produtos';
 
 function App() {
   return (
     <Switch>
+      <Route exact path="/" component={ Home } />
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/products" component={ Produtos } />
     </Switch>
   );
 }
---
+++ front-end/src/components/Menu.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Menu = ({ title }) => {
+  const [menu, setMenu] = useState(false);
+  console.log(menu);
+
+  const cleanLocal = () => {
+    localStorage.clear();
+  };
+
+  const sideMenu = () => {
+    return (
+      <div class="side-menu-container">
+        <Link to="/products">
+          <button data-testid="side-menu-item-products" type="button">
+            Produtos
+          </button>
+        </Link>
+        <hr />
+        <Link to="/orders">
+          <button data-testid="side-menu-item-my-orders" type="button">
+            Meus pedidos
+          </button>
+        </Link>
+        <hr />
+        <Link to="/profile">
+          <button data-testid="side-menu-item-my-profile" type="button">
+            Meu Perfil
+          </button>
+        </Link>
+        <hr />
+        <Link to="login">
+          <button
+            data-testid="side-menu-item-logout"
+            type="button"
+            onClick={cleanLocal}
+          >
+            Sair
+          </button>
+        </Link>
+        <hr />
+      </div>
+    );
+  };
+
+  return (
+    <header>
+      <button
+        data-testid="top-hamburguer"
+        type="button"
+        onClick={() => setMenu(!menu)}
+      >
+        Icone
+      </button>
+      {menu && sideMenu()}
+      <h1 data-testid="top-title">{title}</h1>
+    </header>
+  );
+};
+
+export default Menu;
