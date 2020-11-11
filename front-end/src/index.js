eda58e762ed4fee28a2a32c44e7cd5dc545df1cf
Pedro <pedrotpoliveira@gmail.com>
2020-11-11 17:49:40 -0300

    Initial Setup

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 import Routes from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes />
+      <ThemeProvider>
+        <CSSReset />
+        <Routes />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+} from '@chakra-ui/core';
 
-const Home = () => (
-  <>
-    <Link to="/login">
-      Login
-    </Link>
-    <Link to="/products">
-      Products
-    </Link>
-    <Link to="/register">
-      Register
-    </Link>
-  </>
-);
+const Home = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isActive, setActive] = useState('false');
+  const btnRef = React.useRef();
+
+  const toggleClose = () => {
+    setActive(!isActive);
+  };
+
+  return (
+    <>
+      <Button ref={ btnRef } variantColor="teal" onClick={ toggleClose }>
+        Open
+      </Button>
+      {isActive ? onOpen() : onClose()}
+      <Drawer
+        isOpen={ isOpen }
+        placement="right"
+        onClose={ onClose }
+        finalFocusRef={ btnRef }
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Link to="/login">
+              Login
+            </Link>
+            <Link to="/products">
+              Products
+            </Link>
+            <Link to="/register">
+              Register
+            </Link>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={ 3 } onClick={ onClose }>
+              Cancel
+            </Button>
+            <Button color="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+
+  );
+};
 export default Home;
