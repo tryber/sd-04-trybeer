09ed2ea7082031e274ca2f475be56f294c732d31
Josias Viveiro <josiasviveiro@gmail.com>
2020-11-11 18:10:41 -0300

    Campos de registro

---
+++ front-end/src/pages/Register/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormLabel,
+  // FormErrorMessage,
+  Input,
+} from '@chakra-ui/core';
+
+const Register = () => {
+  return (
+    <div>
+      <FormControl>
+        <FormLabel htmlFor="name">Nome</FormLabel>
+        <Input type='text' name='signName' id='name' data-testid="signup-name" />
+
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <Input type='email' name='signEmail' id="email" data-testid="signup-email" />
+
+        <FormLabel htmlFor="password">Senha</FormLabel>
+        <Input type='password' name='signPassword' id="password" data-testid="signup-password" />
+
+        <Checkbox variantColor="green" data-testid="signup-seller">Quero vender</Checkbox>
+
+        <Button variantColor="green" type='submit' data-testid="signup-btn">Cadastrar</Button>
+      </FormControl>
+    </div>
+  )
+};
+
+export default Register;
