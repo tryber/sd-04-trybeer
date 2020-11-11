c6f51919028ca741a63140406b4c59c12a97b742
frederico-campello <fredfmc33@gmail.com>
2020-11-11 13:55:32 -0300

    Requisito 02

---
+++ front-end/src/pages/Register.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [checked, setChecked] = useState(false);
+
+  const [message, setMessage] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const checkName = (name) => {
+    const regexName = /^[a-z\s]*$/i;
+    return regexName.test(name) && name.length >= 12 ? true : false;
+  };
+
+  const checkEmail = (email) => {
+    const regexEmail = /[A-Z0-9]{1,}@[A-Z0-9]{2,}\.[A-Z0-9]{2,}/i;
+    return regexEmail.test(email);
+  };
+
+  const role = checked ? "administrator" : "client";
+
+  const registerUser = () => {
+    axios
+      .post("http://localhost:3001/register", { name, email, password, role })
+      .then((res) => setRedirect(true))
+      .catch((_error) => setMessage("E-mail already in database."));
+  };
+
+  if (role === "administrator" && redirect)
+    return <Redirect to="/admin/orders" />;
+
+  if (role === "client" && redirect) {
+    return <Redirect to="/products" />;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    registerUser();
+  };
+
+  return (
+    <div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="name">Nome</label>
+        <input
+          type="text"
+          id="name"
+          data-testid="signup-name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          data-testid="signup-email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          data-testid="signup-password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="seller">Quero Vender</label>
+        <input
+          type="checkbox"
+          id="seller"
+          data-testid="signup-seller"
+          defaultChecked={checked}
+          onChange={() => setChecked(!checked)}
+        />
+        <button
+          type="submit"
+          data-testid="signup-btn"
+          disabled={
+            !checkEmail(email) || !checkName(name) || password.length < 6
+          }
+        >
+          Cadastrar
+        </button>
+      </form>
+      {message !== "" && <p>{message}</p>}
+    </div>
+  );
+};
+
+export default Register;
