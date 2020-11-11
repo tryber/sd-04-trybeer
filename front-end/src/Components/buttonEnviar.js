3699cc33ee6d6c347138f7e4fc591a578413ec85
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-11-11 19:25:07 -0300

    adicionado useHistory do react-router-dom

--- front-end/src/Components/buttonEnviar.js
+++ front-end/src/Components/buttonEnviar.js
@@ -1,36 +1,34 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Button, Notification } from 'rbx';
 
 import api from '../services/userApi';
 
 const ButtonEnviar = ({ email, password, isDisabled = true }) => {
-  const [redirect, setRedirect] = useState('');
   const [message, setMessage] = useState('');
+
+  const history = useHistory();
 
   const validaLogin = () => {
     api.login(email, password).then((response) => {
       localStorage.setItem('user', JSON.stringify(response.data));
       if (response.data.role === 'administrator') {
-        setRedirect('admin');
-      } else setRedirect('client');
+        history.push('/admin/orders');
+      } else history.push('/products');
     })
       .catch((err) => setMessage(err.response.data.message));
   };
 
-  if (redirect === 'admin') return <Redirect to="/admin/orders" />;
-  if (redirect === 'client') return <Redirect to="/products" />;
-
   return (
     <div>
-      { message ? <Notification color="danger">{message}</Notification> : '' }
+      { message ? <Notification color="danger">{message}</Notification> : ''}
 
       <Button
         type="Button"
         data-testid="signin-btn"
-        disabled={ isDisabled }
-        onClick={ () => validaLogin() }
+        disabled={isDisabled}
+        onClick={() => validaLogin()}
       >
         ENTRAR
       </Button>
