8a3c9a86564bad24c19fccf4afbbadedc85ca53a
wistyk <leonardoxw@gmail.com>
2020-11-11 19:22:47 -0300

    branch atualizada conforme prs do grupo

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
-import Header from './compoments/Header/index'
-import './App.css';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Login from './components/Login';
+import UserRegister from './components/UserRegister';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={ Header } />
-      </Switch>
-      <Header  />
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/login" component={ Login } />
+      <Route path="/register" component={ UserRegister } />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
