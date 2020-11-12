8097cebd2f1d1381ec63f1e975dde7b2e80ed580
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-11-12 16:27:48 -0300

    CC 05

--- back-end/model/userModel.js
+++ back-end/model/userModel.js
@@ -1,29 +1,28 @@
 const connection = require('./connection');
 
-const findByEmail = async (userEmail) => connection().then((db) =>
-  db
-    .getTable('users')
-    .select()
-    .where('email = :email')
-    .bind('email', userEmail)
-    .execute())
-.then((results) => results.fetchOne())
-.then(([id, name, email, password, role]) => ({
-  id,
-  name,
-  email,
-  password,
-  role,
-}));
+const findByEmail = async (userEmail) => connection().then((db) => db
+  .getTable('users')
+  .select()
+  .where('email = :email')
+  .bind('email', userEmail)
+  .execute())
+  .then((results) => results.fetchOne())
+  .then(([id, name, email, password, role]) => ({
+    id,
+    name,
+    email,
+    password,
+    role,
+  }));
 
 const registerUser = async (name, email, password, role) => {
   if (!findByEmail(email)) throw new Error();
 
   return connection().then((db) => db
-      .getTable('users')
-      .insert(['name', 'email', 'password', 'role'])
-      .values(name, email, password, role)
-      .execute());
+    .getTable('users')
+    .insert(['name', 'email', 'password', 'role'])
+    .values(name, email, password, role)
+    .execute());
 };
 
 module.exports = {
