7b803e7feeec6a441001ce1b3df3c2651dfac9df
leandromarten1 <leandromarten1@gmail.com>
2020-11-12 16:11:13 -0300

    fix pages/index

--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -1,11 +1,5 @@
 import Login from './Login';
-<<<<<<< HEAD
 import UserProfile from './UserProfile';
-
-export { Login, UserProfile };
-=======
 import Register from './Register';
 
-const placeholder = 'a';
-export { Login, placeholder, Register };
->>>>>>> b2dbb5232eefb2105147828866389b181197e0e4
+export { Login, UserProfile, Register };
