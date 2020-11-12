f23945c6bf1b6bcffb8d71de51ab8eef859f0f23
Mateus-Moreira <ashuraw@hotmail.com>
2020-11-12 17:16:47 -0300

    [BACK][LOGIN] - Rota /login

---
+++ back-end/models/dbConnection/index.js
@@ -0,0 +1,26 @@
+const mysqlx = require("@mysql/xdevapi");
+require("dotenv/config");
+
+const config = {
+  user: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: 33060,
+  socketPath: "/var/run/mysqld/mysqld.sock",
+};
+
+let schema;
+const connection = () =>
+  schema
+    ? Promise.resolve(schema)
+    : mysqlx
+        .getSession(config)
+        .then((session) => {
+          schema = session.getSchema("trybeer");
+          return schema;
+        })
+        .catch(() => {
+          process.exit(1);
+        });
+
+module.exports = { connection };
