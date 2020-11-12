270892b7daf4908d07d99c3a5371a4d37dbb58dc
Mateus-Moreira <ashuraw@hotmail.com>
2020-11-12 17:27:01 -0300

    implementado o validateJWT

---
+++ back-end/middlewares/validateJWT/index.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+require("dotenv/config");
+
+const secret = process.env.DB_SECRET || "mysecrettoken";
+
+const validateJWT = (req, res, next) => {
+  try {
+    const { token = "" } = req.cookies || {};
+
+    if (!token) {
+      return res.status(500).json({ message: "token inválido!" });
+    }
+
+    const data = jwt.verify(token, secret);
+
+    if (!data) {
+      return res.status(500).json({ message: "token inválido!" });
+    }
+
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: "token inválido!" });
+  }
+};
+
+module.exports = { validateJWT };
