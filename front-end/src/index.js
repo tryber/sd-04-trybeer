644ca6ba04be16169b980ac5e66c20289097414c
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-11-12 09:52:14 -0300

    little refactor
    just some suggestions

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
       <Routes />
-    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import {
   Checkout, Home, Login, NotFound, Orders, OrdersDetails, Products, Profile, Register,
 } from '../pages';
 
 const Routes = () => (
-  <Switch>
-    <Home exact path="/" />
-    <Checkout path="/checkout" />
-    <Login path="/login" />
-    <Orders exact path="/orders" />
-    <Route path="/orders/:id">
-      <OrdersDetails />
-    </Route>
-    <Orders exact path="/admin/orders" />
-    <Route path="/admin/orders/:id">
-      <OrdersDetails />
-    </Route>
-    <Profile path="/admin/profile" />
-    <Products path="/products" />
-    <Profile path="/profile" />
-    <Register path="/register" />
-    <Route component={ NotFound } />
-  </Switch>
+  <BrowserRouter>
+    <Switch>
+      <Home exact path="/" />
+      <Checkout path="/checkout" />
+      <Login path="/login" />
+      <Orders exact path="/orders" />
+      <OrdersDetails path="/orders/:id" />
+      <Orders exact path="/admin/orders" />
+      <OrdersDetails path="/admin/orders/:id" />
+      <Profile path="/admin/profile" />
+      <Products path="/products" />
+      <Profile path="/profile" />
+      <Register path="/register" />
+      <NotFound path="/" />
+    </Switch>
+  </BrowserRouter>
 );
 export default Routes;
