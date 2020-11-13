f3a48426edf225e4a85626ccac170653df7cb20f
Mateus-Moreira <ashuraw@hotmail.com>
2020-11-13 18:21:35 -0300

    corrigido os erros do controller/login

--- back-end/controllers/login/index.js
+++ back-end/controllers/login/index.js
@@ -1,26 +1,24 @@
-const { createToken } = require("../../middlewares/createJWT");
-const { getByEmail } = require("../../models/users");
+const { createToken } = require('../../middlewares/createJWT');
+const { getByEmail } = require('../../models/users');
 
 const login = async (req, res) => {
   const { email, password } = req.body;
 
-  if (!email || !password)
-    return res
-      .status(401)
-      .json({ message: "É necessário usuário e senha para fazer login" });
+  if (!email || !password) {
+    return res.status(401).json({ message: 'É necessário usuário e senha para fazer login' });
+  }
 
   const user = await getByEmail(email);
 
-  if (!user || user.password !== password)
-    return res
-      .status(401)
-      .json({ message: "Usuário não existe ou senha inválida" });
+  if (!user || user.password !== password) {
+    return res.status(401).json({ message: 'Usuário não existe ou senha inválida' });
+  }
 
   const { password: _, ...userWithoutPassword } = user;
 
   const token = createToken(userWithoutPassword);
 
-  res.cookie("token", token, { httpOnly: true, sameSite: true });
+  res.cookie('token', token, { httpOnly: true, sameSite: true });
   return res.status(200).json({ token });
 };
 
