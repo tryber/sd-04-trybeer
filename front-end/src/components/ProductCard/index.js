36f3fdc8081d678145bcfbb8b282b204533a2b61
Ronald Lima <ronaldazevedolima@gmail.com>
2020-11-13 13:21:08 -0300

    acertando alguns erros de cc

--- front-end/src/components/ProductCard/index.js
+++ front-end/src/components/ProductCard/index.js
@@ -5,17 +5,18 @@ import React, { useEffect, useState } from 'react';
 export default function ProductCard() {
   // variavel para formatacao do price
   const formato = { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' };
-  const { index, name, price, url_image, prodQuantity } = this.props;
+  const { index, name, price, urlImage, prodQuantity } = props;
   const [quantity, setQuantity] = useState(0);
 
   // roda quando o card eh montado, checa/ cria um localstorage
   // checa se o item do card ja esta no localstorage e atribui o a quantidade
   useEffect(() => {
     if (!localStorage.cartItens) localStorage.cartItens = JSON.stringify([]);
-    let storage = JSON.parse(localStorage.cartItens);
+    const storage = JSON.parse(localStorage.cartItens);
     if (storage.length !== 0) {
       storage.map((e) => {
         if (e.name === name) setQuantity(e.quantity);
+        return e;
       });
     }
   }, []);
@@ -24,7 +25,7 @@ export default function ProductCard() {
   // checa se o item ja esta no localstorage, se tiver atualiza a quantidade,
   // se nao tiver adiciona o item
   useEffect(() => {
-    let storage = JSON.parse(localStorage.cartItens);
+    const storage = JSON.parse(localStorage.cartItens);
     const additem = {
       name,
       quantity,
@@ -32,6 +33,7 @@ export default function ProductCard() {
     const newStorage = storage.map((e) => {
       if (e.name === name) {
         e.quantity = quantity;
+        return e;
       }
       storage.push(additem);
     });
@@ -40,27 +42,27 @@ export default function ProductCard() {
 
   return (
     <div>
-      <div data-testid={`${index - 1}-product-price`}>{price.toLocalString('pt-BR', formato)}</div>
+      <div data-testid={ `${index - 1}-product-price` }>{ price.toLocalString('pt-BR', formato) }</div>
       <div>
-        <div data-testid={`${index - 1}-product-img`}>{url_image}</div>
-        <div data-testid={`${index - 1}-product-name`}>{name}</div>
+        <div data-testid={`${index - 1}-product-img`}>{ urlImage }</div>
+        <div data-testid={`${index - 1}-product-name`}>{ name }</div>
       </div>
       <div>
-        <div
-          disabled={{ quantity } === 0 ? true : false}
-          data-testid={`${index - 1}-product-minus`}
-          onClick={() => setQuantity(quantity - 1)}
+        <button
+          disabled={ { quantity } === 0 ? true : false }
+          data-testid={ `${index - 1}-product-minus` }
+          onClick={ () => setQuantity(quantity - 1) }
         >
           -
-        </div>
-        <div data-testid={`${index - 1}-product-qtd`}>quantidade{quantity}</div>
-        <div
-          disabled={{ quantity } === prodQuantity ? true : false}
-          data-testid={`${index - 1}-product-plus`}
-          onClick={() => setQuantity(quantity + 1)}
+        </button>
+        <div data-testid={`${index - 1}-product-qtd`}>quantidade{ quantity }</div>
+        <button
+          disabled={ { quantity } === prodQuantity ? true : false }
+          data-testid={ `${index - 1}-product-plus` }
+          onClick={ () => setQuantity(quantity + 1) }
         >
           +
-        </div>
+        </button>
       </div>
     </div>
   );
