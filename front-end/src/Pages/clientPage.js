7c45b0efb454601c00ef73e69d6291caa3c49718
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-11-13 13:13:47 -0300

    git to push

--- front-end/src/Pages/clientPage.js
+++ front-end/src/Pages/clientPage.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, Image, Title } from 'rbx';
 
-const Client = () => (
-  <div>
-    <h2>Client</h2>
-    <h1>Client</h1>
-  </div>
-);
+import apiProducts from '../services/productApi';
+
+const Client = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    apiProducts.getProducts().then((response) => setProducts(response.data));
+  }, [])
+
+  return (
+    <div>
+      <h2>Cliente - Produtos</h2>
+      {
+        products.map((product) => (
+          <Card key={product.id}>
+            <Card.Image>
+              <Image.Container>
+                {product.url_image}
+              </Image.Container>
+            </Card.Image>
+            <Card.Content>
+              <Title as="p" size={4}>
+                {product.name}
+              </Title>
+              <Title as="p" subtitle size={6}>
+                {product.price}
+              </Title>
+            </Card.Content>
+            <Card.Footer>
+              <Card.Footer.Item as="a" href="#">
+                +
+                </Card.Footer.Item>
+              <Card.Footer.Item as="a" href="#">
+                -
+                </Card.Footer.Item>
+            </Card.Footer>
+          </Card>
+        ))
+      }
+    </div>
+  )
+
+};
 
 export default Client;
