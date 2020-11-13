46d374edd7af8fadfa7cc55805a046c073725919
henrique <hbaetaleite@gmail.com>
2020-11-13 15:38:43 -0300

    conflitos resolvidos

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
 const apiTrybeer = axios.create({
   baseURL: 'http://localhost:3001',
 });
@@ -8,18 +7,12 @@ const loginApi = async (email, password) => apiTrybeer.post('/login', { email, p
 
 const registerApi = async (name, email, password, role) => {
   const result = await apiTrybeer.post('/register', {
-    name, email, password, role,
+    name,
+    email,
+    password,
+    role,
   });
   return result;
 };
 
 export default { loginApi, registerApi };
-=======
-const loginApi = async (email, password) => {
-  return axios.post('http://localhost:3001/login', { email, password });
-};
-
-const getUserByEmail = async (email) => axios.get(`/${email}`);
-
-export default { loginApi, getUserByEmail };
->>>>>>> userProfile
