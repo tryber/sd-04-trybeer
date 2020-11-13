4dd171da670fce7b31ed83328fb07c948f579d73
Pedro Pires <pedrotpoliveira@gmail.com>
2020-11-13 20:33:15 -0300

    Update index.js

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -10,22 +10,22 @@ import {
 } from '@chakra-ui/core';
 
 const Register = () => (
-    <div>
-      <FormControl>
-        <FormLabel htmlFor="name">Nome</FormLabel>
-        <Input type="text" name="signName" id="name" data-testid="signup-name" />
+  <div>
+    <FormControl>
+      <FormLabel htmlFor="name">Nome</FormLabel>
+      <Input type="text" name="signName" id="name" data-testid="signup-name" />
 
-        <FormLabel htmlFor="email">Email</FormLabel>
-        <Input type="email" name="signEmail" id="email" data-testid="signup-email" />
+      <FormLabel htmlFor="email">Email</FormLabel>
+      <Input type="email" name="signEmail" id="email" data-testid="signup-email" />
 
-        <FormLabel htmlFor="password">Senha</FormLabel>
-        <Input type="password" name="signPassword" id="password" data-testid="signup-password" />
+      <FormLabel htmlFor="password">Senha</FormLabel>
+      <Input type="password" name="signPassword" id="password" data-testid="signup-password" />
 
-        <Checkbox variantColor="green" data-testid="signup-seller">Quero vender</Checkbox>
+      <Checkbox variantColor="green" data-testid="signup-seller">Quero vender</Checkbox>
 
-        <Button variantColor="green" type="submit" data-testid="signup-btn">Cadastrar</Button>
-      </FormControl>
-    </div>
+      <Button variantColor="green" type="submit" data-testid="signup-btn">Cadastrar</Button>
+    </FormControl>
+  </div>
 );
 
 export default Register;
