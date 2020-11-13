5b680e1fabdc9e122b9227c2d4ce9a5e17cd615c
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-11-13 19:03:41 -0300

    Css and some improvements

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
-import { Login, UserProfile, Register } from './pages';
+import { UserProfile, FormPage } from './pages';
 import { Header } from './components/Header';
+import LoginForm from './components/forms/LoginForm';
+import RegisterForm from './components/forms/RegisterForm';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
+      <Route exact path="/">
+        <FormPage>
+          <LoginForm />
+        </FormPage>
+      </Route>
       <Route path="/login">
-        <Login />
+        <FormPage>
+          <LoginForm />
+        </FormPage>
       </Route>
       <Route path="/profile">
         <UserProfile />
       </Route>
-      <Route path="/register" component={ Register } />
+      <Route path="/register">
+        <FormPage>
+          <RegisterForm />
+        </FormPage>
+      </Route>
       <Route path="/products">
         <Header>TryBeer</Header>
       </Route>
