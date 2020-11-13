25ba8c8c1c02151302969110846a6504904fd70c
henrique <hbaetaleite@gmail.com>
2020-11-12 22:47:54 -0300

    codeclimate v7

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -4,9 +4,9 @@ const apiTrybeer = axios.create({
   baseURL: 'http://localhost:3001',
 });
 const loginApi = async (email, password) => apiTrybeer.post('/login', { email, password });
-const registerApi = async (name, email, password, role) => (
-  apiTrybeer.post('/register',
-    { name, email, password, role
-    }));
+const registerApi = async (name, email, password, role) => {
+  const result = await apiTrybeer.post('/register', { name, email, password, role });
+  return result;
+};
 
 export default { loginApi, registerApi };
