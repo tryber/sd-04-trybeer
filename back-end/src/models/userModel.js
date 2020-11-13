4e4b8480bdbc4db76b475e51a9b6e771e2d83770
gabriellukke <gabriellukke@outlook.com>
2020-11-13 14:51:49 -0300

    userModel refactored

--- back-end/src/models/userModel.js
+++ back-end/src/models/userModel.js
@@ -1,23 +1,10 @@
 const connection = require('./connection');
 
-/* const getUser = async (name) => {
-  try {
-    const db = await connection();
-    const table = db.getTable('users');
-    const result = await table.select(['name', 'email']).where('name = :name').bind('name', name).execute();
-    const user = result.fetchOne();
-    console.log(user);
-    return user;
-  } catch (err) {
-    console.log(err)
-  }
-} */
-
 const updateUser = async (name, email) => {
   try {
     console.log('Model Data:', name, email);
     const db = await connection();
-    const table = db.getTable('users');
+    const table = await db.getTable('users');
     await table.update()
       .set('name', name)
       .where('email = :email')
