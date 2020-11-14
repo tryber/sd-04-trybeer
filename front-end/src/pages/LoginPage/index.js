05e457f0b01c320413cc2b552db23636ca609ea7
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-11-14 03:48:20 -0300

    Tela de Login finalizada

--- front-end/src/pages/LoginPage/index.js
+++ front-end/src/pages/LoginPage/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Login from '../../components/Login';
+import TopMenu from  '../../components/TopMenu';
 
 const LoginPage = () => {
   return (
     <div>
+      <TopMenu />
       <Login />
     </div>
   )
