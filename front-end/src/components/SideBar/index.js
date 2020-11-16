a8e31287fb1b017cfc86bfb1f4bd38ac57f693c9
igorgbr <igorgphotoarte@gmail.com>
2020-11-16 20:44:40 -0300

    button

--- front-end/src/components/SideBar/index.js
+++ front-end/src/components/SideBar/index.js
@@ -78,10 +78,10 @@ const SideBar = ({ role }) => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav
-      id={identification}
-      className={ `${classes} ${toggle ? 'active' : ''}` }
-      onClick= { () => setToggle(!toggle) }
-      onKeyDown= { () => setToggle(!toggle) }
+      id= { identification }
+      className = { `${classes} ${toggle ? 'active' : ''}` }
+      onClick = { () => setToggle(!toggle) }
+      onKeyDown = { () => setToggle(!toggle) }
     >
       <div data-testid="top-hamburguer" className="toggle-btn">
         <span />
