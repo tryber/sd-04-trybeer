51d6437979599ae592a24ab1a277c1efe3c8dd09
Ronald Lima <ronaldazevedolima@gmail.com>
2020-11-16 16:30:38 -0300

    products back

---
+++ back-end/controllers/products/index.js
@@ -0,0 +1,10 @@
+// controller products
+const productModel = require('../../models/products');
+
+const getAll = async (req, res) => {
+  const products = await productModel.getAllProducts();
+  if (products) return res.status(200).json(products);
+  return res.status(500).json({ message: 'internal error' });
+};
+
+module.exports = { getAll };
--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { login } = require('./controllers/login');
 
 const app = express();
 const port = 3001;
 
+const productController = require('./controllers/products');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.post('/login', login);
+app.get('/products', productController.getAll);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
