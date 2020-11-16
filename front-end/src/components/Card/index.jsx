6e35c13a173e4d433d8fec1f95a62359092f1ab9
AliceAAtalla <aliceatalla36@gmail.com>
2020-11-15 22:20:19 -0300

    Price com virgula

--- front-end/src/components/Card/index.jsx
+++ front-end/src/components/Card/index.jsx
@@ -6,7 +6,7 @@ const Card = ({ img, title, price, index }) => {
         <div key={index} className="card-container">
             <img src={img} alt={title} className="card-img" data-testid={`${index}-product-img`} />
             <span data-testid={`${index}-product-name`} >{title}</span>
-            <span data-testid={`${index}-product-price`} >{`R$ ${price.toFixed(2)}`}</span>
+            <span data-testid={`${index}-product-price`} >{`R$ ${price.toLocaleString("pt-BR", {style: 'currency', currency: 'BRL'})}`}</span>
             <div className="button-container">
               <button data-testid={`${index}-product-minus`} type="button">-</button>
               <span data-testid={`${index}-product-qtd`} >0</span>
