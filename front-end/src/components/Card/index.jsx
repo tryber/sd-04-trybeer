acd5eaac26010ce15e160861dd280d75266f0acb
AliceAAtalla <aliceatalla36@gmail.com>
2020-11-16 22:09:36 -0300

    CSS ver carrinho + 6 testes passando

--- front-end/src/components/Card/index.jsx
+++ front-end/src/components/Card/index.jsx
@@ -9,20 +9,18 @@ const Card = ({ img, title, price, index }) => {
   const IncrementCounterHandler = (title, price) => {
     const itemAdd = { title, price };
 
-    setCart((prevState) => [...prevState, itemAdd]);
+    setCart(prevState => [...prevState, itemAdd]);
 
     const cartStore = getLS('cart');
-    
-    return cartStore ?
-      setLS('cart', [...cartStore, itemAdd])
-    : setLS('cart', [itemAdd]);
+
+    return cartStore ? setLS('cart', [...cartStore, itemAdd]) : setLS('cart', [itemAdd]);
   };
 
-  const DecrementCounterHandler = (title) => {
+  const DecrementCounterHandler = title => {
     if (cart.length < 1) return null;
 
     const removeItem = cart.findIndex(item => item.title === title);
-    
+
     if (removeItem >= 0) {
       cart.splice(removeItem, 1);
       setLS('cart', cart);
@@ -30,32 +28,39 @@ const Card = ({ img, title, price, index }) => {
     }
   };
 
-    return (
-        <div key={index} className="card-container">
-            <img src={img} alt={title} className="card-img" data-testid={`${index}-product-img`} />
-            <span data-testid={`${index}-product-name`} >{title}</span>
-            <span data-testid={`${index}-product-price`} >
-              {`${price.toLocaleString("pt-BR", { style: 'currency', currency: 'BRL' })}`}
-            </span>
-            <div className="button-container">
-              <button
-                data-testid={`${index}-product-minus`}
-                onClick={() => DecrementCounterHandler(title)}
-                type="button">
-                  -
-              </button>
-              <span data-testid={`${index}-product-qtd`} >
-                {!cart ? 0 : cart.filter((item) => item.title === title).length}
-              </span>
-              <button
-                data-testid={`${index}-product-plus`}
-                onClick={() => IncrementCounterHandler(title, price)}
-                type="button">
-                  +
-              </button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div key={index} className="card-container">
+      <img
+        src={img}
+        alt={title}
+        className="card-img"
+        data-testid={`${index}-product-img`}
+      />
+      <span data-testid={`${index}-product-name`}>{title}</span>
+      <span data-testid={`${index}-product-price`}>
+        {`${price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`}
+      </span>
+      <div className="button-container">
+        <button
+          data-testid={`${index}-product-minus`}
+          onClick={() => DecrementCounterHandler(title)}
+          type="button"
+        >
+          -
+        </button>
+        <span data-testid={`${index}-product-qtd`}>
+          {!cart ? 0 : cart.filter(item => item.title === title).length}
+        </span>
+        <button
+          data-testid={`${index}-product-plus`}
+          onClick={() => IncrementCounterHandler(title, price)}
+          type="button"
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- front-end/src/pages/ProductsPage/index.jsx
+++ front-end/src/pages/ProductsPage/index.jsx
@@ -3,28 +3,54 @@ import api from '../../services/api';
 
 import Card from '../../components/Card';
 import Menu from '../../components/Menu';
+import './index.css';
+
+import { getLS } from '../../utils';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
   const [data, setData] = useState([]);
-  
+
   useEffect(() => {
-    (async() => {
+    (async () => {
       const products = await api.productsAPI();
-      console.log('data', products)
+      console.log('data', products);
       setData(products);
-    })()
+    })();
   }, []);
-  
+
+  const cartLocalStorage = cart => {
+    if (!cart) return 0;
+    const total = cart.reduce((acc, curr) => acc + curr.price, 0);
+    return total;
+  };
+
+  const cart = getLS('cart') || [];
+
   return (
-  <>
-    <Menu nomeTela="TryBeer" />
-    <div className="container-general container-cards">
-      {data.map(({ url_image, name, price }, index) => (
-        <Card index={index} img={url_image} title={name} price={price} />
-      ))}
-    </div>
-  </>
-  )
+    <>
+      <Menu nomeTela="TryBeer" />
+      <div className="container-general container-cards">
+        {data.map(({ url_image, name, price }, index) => (
+          <Card index={index} img={url_image} title={name} price={price} />
+        ))}
+      </div>
+      <footer className="footer-cart">
+        <span data-testid="checkout-bottom-btn-value">
+          {`Total: R$ ${cartLocalStorage(cart).toFixed(2).replace('.', ',')}`}
+        </span>
+        {cart.length < 1 ? (
+          <button disabled type="button" data-testid="checkout-bottom-btn">
+            Ver Carrinho
+          </button>
+        ) : (
+          <button type="button" data-testid="checkout-bottom-btn">
+            <Link to="/checkout">Ver Carrinho</Link>
+          </button>
+        )}
+      </footer>
+    </>
+  );
 };
 
 export default Products;
