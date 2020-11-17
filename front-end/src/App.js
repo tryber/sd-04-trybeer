daf46634007368e987d90743f11012abc786e540
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-11-17 15:51:39 -0300

    header and profile css done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { UserProfile, FormPage } from './pages';
 import { Header } from './components/Header';
 import LoginForm from './components/forms/LoginForm';
@@ -10,9 +9,7 @@ function App() {
   return (
     <Switch>
       <Route exact path="/">
-        <FormPage>
-          <LoginForm />
-        </FormPage>
+        <Redirect to="/login" />
       </Route>
       <Route path="/login">
         <FormPage>
--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import SideBar from './SideBar';
+import { Duff, menuIcon } from '../images/index';
+import '../css/header.css';
 
 export function Header({ children }) {
   const userinfo = JSON.parse(localStorage.getItem('user'));
   const [btnBurguer, setBtnBurguer] = useState(
-    userinfo && userinfo.role === 'administrator'
+    userinfo && userinfo.role === 'administrator',
   );
   if (!userinfo) return <Redirect to="/login" />;
   return (
-    <div>
+    <div className="header-container">
       <header>
         <button
           data-testid="top-hamburguer"
-          onClick={() => setBtnBurguer(!btnBurguer)}
+          className="header-menu-icon"
+          onClick={ () => setBtnBurguer(!btnBurguer) }
         >
-          hamburguer
+          <img src={ menuIcon } alt="Hamburguer menu icon" />
         </button>
-        <h1 data-testid="top-title">{children}</h1>
+        <img src={ Duff } alt="duff logo" className="duff-logo" />
+        <span data-testid="top-title" className="header-title">{children}</span>
       </header>
-      {btnBurguer && <SideBar userRole={userinfo.role} />}
+      <SideBar userRole={ userinfo.role } active={btnBurguer}/>
     </div>
   );
 }
