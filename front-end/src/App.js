3e365bdb953c4f5bfd122641879a9deb8a373bff
henrique <hbaetaleite@gmail.com>
2020-11-17 16:11:13 -0300

    add registro de user

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Login from './components/Login';
-// import UserRegister from './components/UserRegister';
+import UserRegister from './components/UserRegister';
 // import UserProfile from './components/UserProfile/index';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      {/* <Route path="/register" component={ UserRegister } /> */}
-      <Route path="/login" component={ Login } />
-      <Route exact path="/" component={ Login } />
+      <Route path="/register" component={UserRegister} />
+      <Route path="/login" component={Login} />
+      <Route exact path="/" component={Login} />
       {/* <Route path="/profile" component={ UserProfile } /> */}
     </Switch>
   </BrowserRouter>
