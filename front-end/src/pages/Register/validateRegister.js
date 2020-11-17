df24bf4613828e6afbd03e31a83de53f1d207b3d
Josias Viveiro <josiasviveiro@gmail.com>
2020-11-17 18:25:54 -0300

    Texto das validações com .required

--- front-end/src/pages/Register/validateRegister.js
+++ front-end/src/pages/Register/validateRegister.js
@@ -6,19 +6,19 @@ let schema = yup.object().shape({
     .string()
     .min(12, 'O nome deve conter pelo menos 12 letras')
     .matches(/^([a-zA-Z ])*$/gmi, 'O Nome deve conter somente letras')
-    .required(),
+    .required('É necessário inserir um nome'),
 
   // email - deve conter um email válido. Um email válido possui o formato <nome>@<domínio>;  
   signEmail: yup
     .string()
     .email('O email deve ter o formato nome@domínio.com')
-    .required(),
+    .required('É necessário inserir um email'),
 
   // senha - composta por, no mínimo, 6 números;
   signPassword: yup
     .string()
     .min(6, 'A senha deve conter pelo menos 6 números')
-    .required(),
+    .required('É necessário inserir uma senha'),
 });
 
 // "Testes" de validação com console.log
