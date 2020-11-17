15bbe319f5965d23028785b31a95372eb78114ce
Pedro T. Pires de Oliveira <pedrotpo@Pedros-MacBook-Air.local>
2020-11-17 19:25:12 -0300

    Alert Component

---
+++ front-end/src/components/Alert/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+} from '@chakra-ui/react';
+
+const Alert = (props) => {
+  const { isOpen, onClose, message } = props;
+  return (
+    <AlertDialog
+      motionPreset="slideInBottom"
+      isCentered
+      isOpen={ isOpen }
+      onClose={ onClose }
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogBody>
+            {message}
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button colorScheme="red" onClick={ onClose } ml={ 3 }>
+              OK
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  );
+};
+
+export default Alert;
