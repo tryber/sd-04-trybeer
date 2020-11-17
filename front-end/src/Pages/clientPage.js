920adfdeade1d8eb48fcd5ac28d735ef6286500c
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-11-17 19:51:00 -0300

    cc 05

--- front-end/src/Pages/clientPage.js
+++ front-end/src/Pages/clientPage.js
@@ -29,30 +29,32 @@ const Client = () => {
       <h3 data-testid="top-title">TryBeer</h3>
       <div className="row row-cols-1 row-cols-md-3">
         {products.map((product, index) => (
-          <div key={product.id} className="col mb-4">
-            <div className="card h-100 border-success" key={product.id}>
+          <div key={ product.id } className="col mb-4">
+            <div className="card h-100 border-success" key={ product.id }>
               <img
-                src={product.url_image}
-                data-testid={`${index}-product-img`}
+                src={ product.url_image }
+                data-testid={ `${index}-product-img` }
                 className="card-img-top img-thumbnail border-success"
-                style={{ width: 400, height: 350 }}
+                style={ { width: 400, height: 350 } }
                 alt="foto do produto"
               />
               <div className="card-body">
-                <h4 className="card-title" data-testid={`${index}-product-name`}>
-                  {product.name}
+                <h4 className="card-title" data-testid={ `${index}-product-name` }>
+                  { product.name }
                 </h4>
                 <h4
                   className="card-title"
-                  data-testid={`${index}-product-price`}
-                >{`R$ ${product.price.toFixed(2).replace('.', ',')}`}</h4>
+                  data-testid={`${ index}-product-price` }
+                >
+                  { `R$ ${product.price.toFixed(2).replace('.', ',')}` }
+                </h4>
               </div>
               <div className="card-footer border-success">
                 <ShopCart
-                  product={product}
-                  setCartUpdate={setCartUpdate}
-                  cartUpdate={cartUpdate}
-                  index={index}
+                  product={ product }
+                  setCartUpdate={ setCartUpdate }
+                  cartUpdate={ cartUpdate }
+                  index={ index }
                 />
               </div>
             </div>
@@ -60,10 +62,10 @@ const Client = () => {
         ))}
       </div>
       <footer>
-        <button type='button' disabled={valorTotal ? false : true} data-testid="checkout-bottom-btn">
-          <Link to='/checkout'>
+        <button type="button" disabled={ valorTotal ? false : true } data-testid="checkout-bottom-btn">
+          <Link to="/checkout">
             <span data-testid="checkout-bottom-btn-value">
-              {`Ver Carrinho R$ ${valorTotal.toFixed(2).replace('.', ',')}`}
+              { `Ver Carrinho R$ ${valorTotal.toFixed(2).replace('.', ',')}` }
             </span>
           </Link>
         </button>
