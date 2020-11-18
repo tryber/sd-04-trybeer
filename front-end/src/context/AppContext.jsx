df473964c69d06f1ff3ccf1681e088f2d7c2e9ef
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2020-11-18 17:26:26 -0300

    req 6 checkout pages

--- front-end/src/context/AppContext.jsx
+++ front-end/src/context/AppContext.jsx
@@ -1,17 +1,27 @@
-import React, { createContext, useState } from "react";
-import PropTypes from "prop-types";
+import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const AppContext = createContext();
 
+const list = [
+  { product: 'caipirinha', qty: 4, value: 10.0 },
+  { product: 'heineken', qty: 10, value: 15.0 },
+  { product: 'Conhaque', qty: 2, value: 12.5 },
+  { product: 'Hennesey', qty: 2, value: 50.0 },
+];
+
 const AppProvider = ({ children }) => {
-  const [email, setEmail] = useState("jctinin@outlook.com");
-  const [userName, setUserName] = useState("Julio Cesar Tinin");
+  const [email, setEmail] = useState('jctinin@outlook.com');
+  const [userName, setUserName] = useState('Julio Cesar Tinin');
+  const [orderList, setOrderList] = useState(list);
 
   const context = {
     email,
     setEmail,
     userName,
     setUserName,
+    orderList,
+    setOrderList,
   };
 
   return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
--- front-end/src/pages/CloseOrder.jsx
+++ front-end/src/pages/CloseOrder.jsx
@@ -1,10 +1,110 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import TopBar from '../components/ClientBar.jsx';
+import { AppContext } from '../context/AppContext';
 
-const CloseOrder = () => (
-  <div>
-    <TopBar title={'Finalizar Pedido'} isAdm={false} />
-  </div>
-);
+const CloseOrder = () => {
+  const { orderList, setOrderList } = useContext(AppContext);
+  const [message, setMessage] = useState(null);
+
+  const orderRef = useRef(null);
+
+  useEffect(() => {
+    makeTotalValue(orderList);
+  }, []);
+
+  const makeTotalValue = (orderList) => {
+    let totalPrice = document.getElementById('itemTotal');
+
+    if (orderList.length > 0) {
+      const total = orderList
+        .map(({ value, qty }) => {
+          return value * qty;
+        })
+        .reduce((acc, curr) => acc + curr);
+      totalPrice.innerText = 'Total: R$' + total;
+    } else {
+      totalPrice.innerHTML = 'Total: R$ 0';
+      setMessage('Não há produtos no carrinho');
+    }
+  };
+
+  const removeItemFromArray = (product) => {
+    let newArr = orderList.filter(
+      (value, index, arr) => value.product !== product
+    );
+    setOrderList(newArr);
+    makeTotalValue(newArr);
+  };
+
+  return (
+    <div>
+      <TopBar title={'Finalizar Pedido'} isAdm={false} />
+
+      <h1>Produtos</h1>
+      <p>{message}</p>
+      <ul ref={orderRef} id="list" className="list-group">
+        {orderList.map(({ product, qty, value }, index) => {
+          return (
+            <li
+              id={product}
+              key={product}
+              className="list-group-item list-group-item-action list-group-item-primary"
+              index={index}
+            >
+              <div className="container">
+                <div className="row">
+                  <div data-testid="0-product-qtd-input" className="col">
+                    {qty}
+                  </div>
+                  <div data-testid="0-product-name" className="col-6">
+                    {product}
+                  </div>
+                  <div data-testid="0-product-unit-price" className="col">
+                    {value}
+                  </div>
+                  <div
+                    onClick={(e) => removeItemFromArray(product)}
+                    className="col"
+                  >
+                    <button
+                      data-testid="0-removal-button"
+                      className="btn btn-danger"
+                    >
+                      X
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      <p data-testid="order-total-value" id="itemTotal">
+        Total: R${' '}
+      </p>
+      <div className="col-md-6 offset-md-6">
+        <form method="POST" action="">
+          <h2>Endereço</h2>
+          <label htmlFor="rua">Rua:</label>
+          <input
+            data-testid="checkout-street-input"
+            type="text"
+            className="input-group-prepend"
+          />
+          <br />
+          <label htmlFor="numeroCasa" className="input-group-prepend">
+            Número da casa:
+          </label>
+          <input data-testid="checkout-house-number-input" type="text" />
+        </form>
+        <br />
+        <br />
+        <button data-testid="checkout-finish-btn" className="btn btn-success">
+          Finalizar compra
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default CloseOrder;
