688ba77a01f18a4777e8970c23599cedca569378
Josias Viveiro <josiasviveiro@gmail.com>
2020-11-18 13:50:48 -0300

    Colocando tryCatch no controller/register

--- back-end/controllers/register/index.js
+++ back-end/controllers/register/index.js
@@ -1,21 +1,26 @@
 const { add, getByEmail } = require('../../models/users');
 
 const register = async (req, res) => {
-  const {signName, signEmail, signPassword, signRole} = req.body;
-  // signRole é boolean
-  // signRole - true -> 'administrator'
-  // signRole - false -> 'client'
-  const role = signRole ? 'administrator' : 'client';
+  try {
+    const {signName, signEmail, signPassword, signRole} = req.body;
+    // signRole é boolean
+    // signRole - true -> 'administrator'
+    // signRole - false -> 'client'
+    const role = signRole ? 'administrator' : 'client';
 
-  // Verifica se existe usuário com o mesmo email
-  const user = await getByEmail(signEmail);
-  if(user) {
-    res.status().json({ message: 'E-mail already in database.' });
+    // Verifica se existe usuário com o mesmo email já existe
+    const user = await getByEmail(signEmail);
+    if(user) {
+      res.status(200).json({ message: 'E-mail already in database.' });
+    }
+
+    await add(signName, signEmail, signPassword, role);
+    const newUser = await getByEmail(signEmail);
+    res.status(201).json(newUser);
+  } catch (_error) {
+    console.log(_error.message);
+    res.status(501).json({ message: 'Falha ao cadastrar usuário' });
   }
-
-  await add(signName, signEmail, signPassword, role);
-  const newUser = await getByEmail(signEmail);
-  res.status().json(newUser);
 };
 
 module.exports = { register };
