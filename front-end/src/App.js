057d3d13d7206cb7347289978e7e060ab5b01f8d
wistyk <leonardoxw@gmail.com>
2020-11-18 19:01:28 -0300

    correçao de rota

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,7 +16,7 @@ const App = () => (
       <Route path="/login" component={ Login } />
       <Route exact path="/" component={ Login } />
       <Route path="/profile" component={ UserProfile } />
-      <Route path="/admin/profile" component={ AdminProfile } />
+      <Route path="/admin/orders" component={ AdminProfile } />
     </Switch>
   </BrowserRouter>
 );
