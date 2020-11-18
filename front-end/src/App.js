4214bf5ee03931e3f6e61ca4d0865f4f5ba6296a
wistyk <leonardoxw@gmail.com>
2020-11-18 16:14:56 -0300

    CC

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import Login from './components/Login';
 import UserProfile from './components/UserProfile/index';
 import AdminProfile from './components/AdminProfile/index';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/login" component={ Login } />
-      <Route exact path="/" component={ Login } />
       <Route path="/profile" component={ UserProfile } />
       <Route path="/admin/profile" component={ AdminProfile } />
     </Switch>
