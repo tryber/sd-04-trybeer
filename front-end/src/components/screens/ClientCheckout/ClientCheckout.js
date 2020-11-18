72e71c0ea7e4196ebc8c72f67ee5a2ff45e681d4
wistyk <leonardoxw@gmail.com>
2020-11-18 18:54:27 -0300

    header aplicada na tela de checkout ok nos testes

--- front-end/src/components/screens/ClientCheckout/ClientCheckout.js
+++ front-end/src/components/screens/ClientCheckout/ClientCheckout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../../Header';
+import SideBar from '../../SideBar';
 
 // Componente genérico para auxiliar nos testes do req 5, sintam-se
 // livres para substituí-lo
 const ClientCheckout = () => (
   <div>
-    <h1>Checkout</h1>
+    <Header title="Checkout" />
   </div>
 );
 
