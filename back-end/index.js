aa542efd440f30da179578c281904989780d733e
Ronald Lima <ronaldazevedolima@gmail.com>
2020-11-19 15:03:36 -0300

    colocando o jwt no lugar certo agora

--- back-end/index.js
+++ back-end/index.js
@@ -12,9 +12,9 @@ const port = 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/images', validateJWT, express.static('images'));
+app.use('/images', express.static('images'));
 
 app.post('/login', login);
-app.get('/products', productController.getAll);
+app.get('/products', validateJWT, productController.getAll);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
