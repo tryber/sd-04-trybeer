f8c84f68db3862ea0704d8839dd45d386d2ae5aa
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-19 03:02:44 -0300

    Validaçoes feitas no register

--- front-end/src/validation/index.js
+++ front-end/src/validation/index.js
@@ -1,9 +1,15 @@
-const NAME_PATTERN = /[a-zA-Z ]{12,}/;
+const NAME_PATTERN = /^[a-zA-Z]+$/;
 const EMAIL_PATTERN = /[A-Z0-9]{1,}@[A-Z0-9]{2,}\.[A-Z0-9]{2,}/i;
 const PASS_LENGTH = 6;
+
+const LENGTH = 12;
+const registerValidation = (name, email, pass) => (
+  name.length < LENGTH || pass === '' || !NAME_PATTERN.test(name) || !EMAIL_PATTERN.test(email)
+);
 
 module.exports = {
   NAME_PATTERN,
   EMAIL_PATTERN,
   PASS_LENGTH,
+  registerValidation,
 };
