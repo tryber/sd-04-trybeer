a231a6e8ee935ac4e6b90e5f1f739993cd6cb640
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-11-18 22:21:52 -0300

    lint fix

--- front-end/src/images/index.js
+++ front-end/src/images/index.js
@@ -3,4 +3,4 @@ export { default as Duff } from './Duff.png';
 export { default as Beer } from './Beer.png';
 export { default as menuIcon } from './menuIcon.png';
 export { default as logout } from './logout.png';
-export {default as loading } from './loading.gif';
+export { default as loading } from './loading.gif';
