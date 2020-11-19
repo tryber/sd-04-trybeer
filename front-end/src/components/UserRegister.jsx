dc673971031f7a6c58409aab06c568572df817a2
wistyk <leonardoxw@gmail.com>
2020-11-19 19:36:13 -0300

    userRegister alinhado com codigo do Baeta, pendente direcionamento de usuario cadastrado, mas nao logado

--- front-end/src/components/UserRegister.jsx
+++ front-end/src/components/UserRegister.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import API from '../services/api';
+
 const UserRegister = () => {
   const history = useHistory();
 
@@ -11,6 +13,8 @@ const UserRegister = () => {
   const [password, setPassword] = useState('');
 
   const [checkbox, setCheckbox] = useState('');
+
+  const [errMsg, setErrMsg] = useState('');
 
   const isNameValid = (name = '') => name.match(/^([a-zA-Zà-úÀ-Ú]|\s+)+$/);
 
@@ -28,76 +32,90 @@ const UserRegister = () => {
 
   const disableButton = !nameValidated() || !password || password.length < 6 || !emailValidated();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const role = checkbox === 'on' ? 'administrator' : 'client';
+
+    const apiResult = await API.registerApi(name, email, password, role);
+
+    if (apiResult.data.err) return setErrMsg(apiResult.data.err);
+
+    setErrMsg('');
+
     if (checkbox === 'on') return history.push('/admin/orders');
-    return history.push('/products');
+
+    if (checkbox !== 'on') return history.push('/products');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <section className="card w-75 mx-auto m-3">
-        <div className="form-group w-75 mx-auto m-2">
-          <label htmlFor="name">Nome</label>
-          <input
-            data-testid="signup-name"
-            id="name"
-            name="name"
-            type="text"
-            onChange={(e) => setName(e.target.value)}
-            minLength="12"
-            className="form-control"
-          />
-        </div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <section className="card w-75 mx-auto m-3">
+          <div className="form-group w-75 mx-auto m-2">
+            <label htmlFor="name">Nome</label>
+            <input
+              data-testid="signup-name"
+              id="name"
+              name="name"
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              minLength="12"
+              className="form-control"
+            />
+          </div>
 
-        <div className="form-group w-75 mx-auto m-2">
-          <label htmlFor="email">Email</label>
-          <input
-            data-testid="signup-email"
-            id="email"
-            name="email"
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            className="form-control"
-          />
-        </div>
+          <div className="form-group w-75 mx-auto m-2">
+            <label htmlFor="email">Email</label>
+            <input
+              data-testid="signup-email"
+              id="email"
+              name="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+            />
+          </div>
 
-        <div className="form-group w-75 mx-auto m-2">
-          <label htmlFor="password">Password</label>
-          <input
-            data-testid="signup-password"
-            id="password"
-            name="password"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            minLength="6"
-            className="form-control"
-          />
-        </div>
+          <span className="mx-auto m-3 text-danger">{errMsg}</span>
 
-        <div className="form-group w-75 mx-auto m-2">
-          <input
-            data-testid="signup-seller"
-            id="sell"
-            name="sell"
-            type="checkbox"
-            onChange={(e) => setCheckbox(e.target.value)}
-          />
-          <label htmlFor="sell" className="m-2">
-            Quero Vender
-          </label>
-        </div>
+          <div className="form-group w-75 mx-auto m-2">
+            <label htmlFor="password">Password</label>
+            <input
+              data-testid="signup-password"
+              id="password"
+              name="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              minLength="6"
+              className="form-control"
+            />
+          </div>
 
-        <button
-          data-testid="signup-btn"
-          type="submit"
-          disabled={disableButton}
-          className="btn btn-warning w-50 mx-auto m-2"
-        >
-          Cadastrar
-        </button>
-      </section>
-    </form>
+          <div className="form-group w-75 mx-auto m-2">
+            <input
+              data-testid="signup-seller"
+              id="sell"
+              name="sell"
+              type="checkbox"
+              onChange={(e) => setCheckbox(e.target.value)}
+            />
+            <label htmlFor="sell" className="m-2">
+              Quero Vender
+            </label>
+          </div>
+
+          <button
+            data-testid="signup-btn"
+            type="submit"
+            disabled={disableButton}
+            className="btn btn-warning w-50 mx-auto m-2"
+          >
+            Cadastrar
+          </button>
+        </section>
+      </form>
+    </div>
   );
 };
 
