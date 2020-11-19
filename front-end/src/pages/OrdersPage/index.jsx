0f33d500fbced3b81decba0a763dd5ac97e2aaec
AliceAAtalla <aliceatalla36@gmail.com>
2020-11-18 22:39:55 -0300

    CSS nao me obedece

--- front-end/src/pages/OrdersPage/index.jsx
+++ front-end/src/pages/OrdersPage/index.jsx
@@ -4,35 +4,38 @@ import Menu from '../../components/Menu';
 import OrderCard from '../../components/OrderCard/OrderCard';
 import api from '../../services/api';
 import { getLS } from '../../utils';
-import './index.css'; 
-
+import './index.css';
 
 const Orders = () => {
-  const [ loading, setLoading ] = useState(false);
-  const [ saless, setSales ] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [saless, setSales] = useState([]);
 
   useEffect(() => {
     setLoading(true);
-    api.getSalesTb().then((data) => setSales(data.data.sales));
+    api.getSalesTb().then(data => setSales(data.data.sales));
     setLoading(false);
   }, []);
 
-
-  if(!getLS('user')) return <Redirect to='/' />
-  return loading ? <h1>Carregando...</h1> : (
+  if (!getLS('user')) return <Redirect to="/" />;
+  return loading ? (
+    <h1>Carregando...</h1>
+  ) : (
     <div>
       <Menu nomeTela="Meus Pedidos" />
-      <div className="page-container">
+      <div className="page-container margin-top">
         <h3>Sua cesta</h3>
         <div className="products">
-          {saless ? saless.map((order, index) => <OrderCard
-            id={order.id}
-            totalPrice={order.totalPrice}
-            saleDate={order.saleDate}
-            index={index}
-            key={index}
-            />
-          ) : ''}
+          {saless
+            ? saless.map((order, index) => (
+                <OrderCard
+                  id={order.id}
+                  totalPrice={order.totalPrice}
+                  saleDate={order.saleDate}
+                  index={index}
+                  key={index}
+                />
+              ))
+            : ''}
         </div>
       </div>
     </div>
