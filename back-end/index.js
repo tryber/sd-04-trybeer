eac271d67ddcc519d7865186e0477c7b291aa5a4
frederico-campello <fredfmc33@gmail.com>
2020-11-19 16:50:18 -0300

    Requisito 08 - Valeu Renato, Vini & Grupo 08

--- back-end/index.js
+++ back-end/index.js
@@ -131,7 +131,6 @@ app.get('/sales', async (req, res) => {
 app.get('/details', async (req, res) => {
   try {
     const { saleId } = req.query;
-    console.log(saleId);
     const orderDetails = await salesProductsModel.getSaleById(saleId);
     return res.status(200).json(orderDetails);
   } catch (error) {
--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
@@ -6,8 +7,13 @@ import Menu from '../components/Menu';
 
 const OrderDetails = () => {
   const [orderDetails, setOrderDetails] = useState([]);
+  const [redirectToLogin, setRedirectToLogin] = useState(false);
 
   useEffect(() => {
+    if (!localStorage.getItem('user')) {
+      return setRedirectToLogin(true);
+    }
+
     axios
       .get('http://localhost:3001/details', {
         params: { saleId: window.location.pathname.slice(8) },
@@ -21,6 +27,37 @@ const OrderDetails = () => {
   return (
     <div>
       <Menu title="Detalhes de Pedido" />
+      {redirectToLogin && <Redirect to="/login" />}
+      {orderDetails.length && (
+        <div>
+          <p data-testid="order-number">{`Pedido ${orderDetails[0].saleID}`}</p>
+          <p data-testid="order-date">
+            {new Date(orderDetails[0].saleDate)
+              .toLocaleDateString('pt-BR', { timeZone: 'UTC' })
+              .slice(0, 5)}
+          </p>
+          <p data-testid="order-total-value">{`Total: R$ ${orderDetails[0].totalPrice
+            .toFixed(2)
+            .replace('.', ',')}`}</p>
+        </div>
+      )}
+      {orderDetails && (
+        <ol>
+          {orderDetails.map((order, index) => (
+            <li key={order.productName}>
+              <p data-testid={`${index}-product-name`}>{order.productName}</p>
+              <p data-testid={`${index}-product-qtd`}>
+                {order.productQuantity}
+              </p>
+              <p data-testid={`${index}-product-total-value`}>
+                {`R$ ${(order.productPrice * order.productQuantity)
+                  .toFixed(2)
+                  .replace('.', ',')}`}
+              </p>
+            </li>
+          ))}
+        </ol>
+      )}
     </div>
   );
 };
