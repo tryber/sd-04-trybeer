d591cc3b53722b57e16c11690a21151d5b286779
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-11-19 16:55:07 -0300

    cc 04 -.-

--- back-end/model/orderModel.js
+++ back-end/model/orderModel.js
@@ -7,26 +7,25 @@ const createOrder = async (
   deliveryNumber,
   saleDate,
   status = 'Pendente',
-) => connection().then((db) =>
-      db
-        .getTable('sales')
-        .insert([
-          'user_id',
-          'total_price',
-          'delivery_address',
-          'delivery_number',
-          'sale_date',
-          'status',
-        ])
-        .values(
-          userId,
-          totalPrice,
-          deliveryAddress,
-          deliveryNumber,
-          saleDate,
-          status,
-        )
-        .execute());
+) => connection().then((db) => db
+  .getTable('sales')
+  .insert([
+    'user_id',
+    'total_price',
+    'delivery_address',
+    'delivery_number',
+    'sale_date',
+    'status',
+  ])
+  .values(
+    userId,
+    totalPrice,
+    deliveryAddress,
+    deliveryNumber,
+    saleDate,
+    status,
+  )
+  .execute());
 
 module.exports = {
   createOrder,
