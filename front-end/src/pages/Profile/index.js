cd2e2637f4a6f1c089c6452a5a8bc6d38740b874
Pedro T. Pires de Oliveira <pedrotpo@Pedros-MacBook-Air.local>
2020-11-20 15:37:56 -0300

    Starting Profile

--- front-end/src/pages/Profile/index.js
+++ front-end/src/pages/Profile/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
-const Profile = () => (
-  <p>This is the profile page</p>
-);
+const Profile = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!localStorage.user) history.push('/login');
+    const userRole = jwtDecode(localStorage.user);
+  }, []);
+
+  return (
+    <p>
+      This is the
+      {' '}
+      {userRole}
+      {' '}
+      profile page
+    </p>
+  );
+};
 export default Profile;
