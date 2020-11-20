ddcfe0422573d6cbf31bc8d6d0e5b6e98a123023
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-11-19 22:51:45 -0300

    tela perfil admin

--- front-end/src/Pages/adminProfile.js
+++ front-end/src/Pages/adminProfile.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import AdminTopMenu from '../Components/Menu/admin/AdminTopMenu';
 
-const Admin = () => (
-  <div>
-    <AdminTopMenu title="Admin - Perfil" />
-  </div>
-);
+import './styleClientPage.css';
 
-export default Admin;
+function AdminProfile() {
+  const data = JSON.parse(localStorage.getItem('user'));
+  if (!data || data.role !== 'admin') return <Redirect to="/login" />;
+  return (
+    <div>
+      <AdminTopMenu title="Admin - Perfil" />
+      <div className="container">
+        <h2>Perfil</h2>
+        <h3 data-testid="profile-name">
+          Nome:
+          {data.name}
+        </h3>
+        <h3 data-testid="profile-email">
+          Email:
+          {data.email}
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default AdminProfile;
