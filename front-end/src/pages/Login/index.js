65566b3faeed37d68a92ef84e71056d2026bc956
Pedro T. Pires de Oliveira <pedrotpo@Pedros-MacBook-Air.local>
2020-11-20 14:31:15 -0300

    Implemented refacored ls logic w/ jwt-decode

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -8,6 +8,7 @@ import {
   Input,
 } from '@chakra-ui/react';
 import { useFormik } from 'formik';
+import jwtDecode from 'jwt-decode';
 
 import { userLogin } from '../../api';
 import validationSchema from './validateLogin';
@@ -34,7 +35,8 @@ const Login = () => {
         formik.resetForm();
         return null;
       }
-      const redirect = result.data.role === 'client' ? '/products' : '/admin/orders';
+      if (!localStorage.user) localStorage.user = JSON.stringify(result.data);
+      const redirect = jwtDecode(result.data).role === 'client' ? '/products' : '/admin/orders';
       return history.push(redirect);
     },
   });
