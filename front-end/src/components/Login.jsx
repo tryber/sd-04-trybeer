534b96eb6bf80f541edece9993d92db00495fa5b
danilossousa <danilosantossousa@gmail.com>
2020-11-20 02:57:04 -0300

    correçao await 4

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -37,7 +37,6 @@ const Login = () => {
 
     setErrorMsg('');
 
-    console.log(api)
     if (api.data.user.role === 'client') return history.push('/products');
 
     return history.push('/admin/orders');
--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -4,11 +4,7 @@ const apiTrybeer = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
-const loginApi = async (email, password) => {
-  const result = await apiTrybeer.post('/login', { email, password });
-
-  return result;
-};
+const loginApi = (email, password) => apiTrybeer.post('/login', { email, password });
 
 // const getUserByEmail = async (email) => axios.get(`/${email}`);
 
