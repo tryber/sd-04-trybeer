34f17a34e0c60681bd28c0a81eaa9af8a2f0e7f7
wistyk <leonardoxw@gmail.com>
2020-11-20 11:17:41 -0300

    alterado rota de userProfile para privada

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,7 +16,7 @@ const App = () => (
       <Route path="/register" component={ UserRegister } />
       <Route path="/login" component={ Login } />
       <Route exact path="/" component={ Login } />
-      <Route path="/profile" component={ UserProfile } />
+      <PrivateRoute path="/profile" component={ UserProfile } />
       <PrivateRoute path="/admin/profile" component={ AdminProfile } />
     </Switch>
   </BrowserRouter>
