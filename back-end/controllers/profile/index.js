cf8c6acda9cb284f131ce78634b0f839df236790
Mateus-Moreira <ashuraw@hotmail.com>
2020-11-20 17:43:58 -0300

    update profileControler

--- back-end/controllers/profile/index.js
+++ back-end/controllers/profile/index.js
@@ -1,13 +1,16 @@
+const { updateUser } = require('../../models/users');
+
 const userUpdate = async (req, res) => {
   const { id } = req.user;
   const { name } = req.body;
-  const user = { id, name };
+  const user = { id, name }
   try {
-    await userModel.updateUser(user);
+    await updateUser(user);
     res.status(200).json({ message: 'Usuário atualizado com sucesso' });
   } catch {
     res.status(500).json({ message: 'Falha ao atualizar o usuário' });
   }
+
 };
 
 module.exports = { userUpdate };
