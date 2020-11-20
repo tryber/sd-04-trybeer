7f3a02fe5d7b0f13d6789502002543e53bd8e78a
lourenco <loulage@gmail.com>
2020-11-20 19:34:47 -0300

    resolvendo bugs da api

--- back-end/controllers/checkoutController.js
+++ back-end/controllers/checkoutController.js
@@ -1,23 +1,29 @@
+// const saleModel = require('../models/saleModel');
 const saleService = require('../services/saleService');
-const salesProductsSerivce = require('../services/salesProductsService');
+const salesProductsService = require('../services/salesProductsService');
 
 const checkoutController = async (req, res) => {
-  const { total, adress, number, date } = req.body;
-  const { productId, quantity } = req.body.product;
-  const { id } = req.user;
+  const { total, address, number, date, products } = req.body;
+  const id = req.user;
+  const convertedDate = new Date(date).toISOString()
+    .replace('T', ' ')
+    .replace('Z', '');
   try {
     const registeredSale = await saleService.registerSaleService(
       id,
       total,
-      adress,
+      address,
       number,
-      date,
+      convertedDate,
     );
-    await salesProductsSerivce.registerSalesProductsService(
-      registeredSale.saleId,
-      productId,
-      quantity,
-    );
+
+    for (let i = 0; i < products.length; i++) {
+      salesProductsService.registerSalesProductsService(
+        registeredSale,
+        products[i].productId,
+        products[i].quantity,
+      );
+    }
 
     return res.status(201).json({ message: 'Compra realizada com sucesso!' });
   } catch (_err) {
