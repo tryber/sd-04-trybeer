2a8b7a4ed2c3acd809756634e093c5e0818731f7
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-11-21 19:52:50 -0300

    CC again

--- back-end/model/productModel.js
+++ back-end/model/productModel.js
@@ -1,37 +1,36 @@
 const connection = require('./connection');
 
-const getAllProducts = async () =>
-  connection()
-    .then((db) => db.getTable('products').select().execute())
-    .then((results) => results.fetchAll())
-    .then((beer) =>
-      beer.map(([id, name, price, urlImage]) => ({
-        id,
-        name,
-        price,
-        urlImage,
-      })),
-    );
+const getAllProducts = async () => connection()
+  .then((db) => db
+    .getTable('products')
+    .select()
+    .execute())
+  .then((results) => results.fetchAll())
+  .then((beer) => beer.map(([id, name, price, urlImage]) => ({
+    id,
+    name,
+    price,
+    urlImage,
+  })));
 
-const getOrderByUserId = async (userId) =>
-  connection()
-    .then((db) =>
-      db.getTable('sales').select().where('user_id = :user_id').bind('user_id', userId).execute(),
-    )
-    .then((results) => results.fetchAll())
-    .then((order) =>
-      order.map(
-        ([id, user_id, total_price, delivery_address, delivery_number, sale_date, status]) => ({
-          id,
-          user_id,
-          total_price,
-          delivery_address,
-          delivery_number,
-          sale_date,
-          status,
-        }),
-      ),
-    );
+const getOrderByUserId = async (userId) => connection().then((db) => db
+  .getTable('sales')
+  .select()
+  .where('user_id = :user_id')
+  .bind('user_id', userId)
+  .execute())
+  .then((results) => results.fetchAll())
+  .then((order) => order.map(([
+    id, user_id, total_price, delivery_address, delivery_number, sale_date, status
+  ]) => ({
+    id,
+    user_id,
+    total_price,
+    delivery_address,
+    delivery_number,
+    sale_date,
+    status,
+  })));
 
 module.exports = {
   getAllProducts,
