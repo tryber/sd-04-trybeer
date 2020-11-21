156b38effa17b2cf3337ddaa7072a6fe48b47a5c
AliceAAtalla <aliceatalla36@gmail.com>
2020-11-21 11:34:54 -0300

    Passando separadamente os testes

--- front-end/src/pages/AdminProfile/index.jsx
+++ front-end/src/pages/AdminProfile/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import MenuAdmin from '../../components/Menu/MenuAdmin';
+import { Redirect } from 'react-router-dom';
+import Menu from '../../components/AdminMenu';
 import { getLS } from '../../utils/index'
 
 
 function AdminProfile() {
   const user = getLS('user');
-  console.log('user', user)
-  const history = useHistory();
-  if(!user || user.role !== "administrator") history.push('/login');
+  // const history = useHistory();
+
+  if(user === null || user.role !== "administrator") return <Redirect to='/login' />;
   return (
     <div>
-      <MenuAdmin title="Meu perfil" />
+    {console.log('user', user)}
+      <Menu />
       <div style={ { display: 'flex', flexDirection: 'column', marginTop: '150px' } }>
           <h2>Perfil</h2>
           <p data-testid="profile-name">Nome: {user.name}</p>
