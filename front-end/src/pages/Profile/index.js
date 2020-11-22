80f29fc15088a97c533312e4ed63e91b6064856b
Pedro T. Pires de Oliveira <pedrotpo@Pedros-MacBook-Air.local>
2020-11-22 19:19:38 -0300

    Implemented form w/o Chakra

--- front-end/src/pages/Profile/index.js
+++ front-end/src/pages/Profile/index.js
@@ -4,55 +4,71 @@ import { Formik } from 'formik';
 import { Button } from '@chakra-ui/react';
 import jwtDecode from 'jwt-decode';
 
+import { userUpdate } from '../../api';
+import Alert from '../../components/Alert';
+
 const Profile = () => {
   const history = useHistory();
-  const [user, setUser] = useState({});
-  const [isAdmin, setIsAdmin] = useState(true);
+  const [localUser, setLocalUser] = useState({});
+  const [hasChanged, setHasChanged] = useState(true);
+  const [alertMsg, setAlertMsg] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
 
   useEffect(() => {
     if (!localStorage.user) history.push('/login');
-    setUser(jwtDecode(localStorage.user));
-    if (jwtDecode(localStorage.user).role === 'client') setIsAdmin(false);
-  }, [isAdmin, history]);
-  return (
-    <Formik
-      enableReinitialize
-      initialValues={ {
-        profileName: user.name,
-        profileEmail: user.email,
-      } }
+    setLocalUser(jwtDecode(localStorage.user));
+  }, [hasChanged, history]);
 
-      onSubmit={ async (/* values */) => {
-        // await userLogin(values.loginEmail, values.loginPassword
-      } }
-    >
-      {({
-        isSubmitting, handleSubmit, handleChange, handleBlur, values, errors,
-      }) => (
-        <form onSubmit={ handleSubmit }>
-          <input
-            type="text"
-            onChange={ handleChange }
-            onBlur={ handleBlur }
-            value={ values.profileName }
-            data-testid="profile-name"
-            disabled={ isAdmin }
-            name="profileName"
-          />
-          <br />
-          <input
-            type="text"
-            value={ values.profileEmail }
-            data-testid="profile-email"
-            disabled
-            name="profileEmail"
-          />
-          <br />
-          {errors.name && <div id="feedback">{errors.name}</div>}
-          <Button type="submit" disabled={ isSubmitting }>Submit</Button>
-        </form>
-      )}
-    </Formik>
+  return (
+    <>
+      <Alert isOpen={ isOpen } onClose={ onClose } message={ alertMsg } />
+      <Formik
+        enableReinitialize
+        initialValues={ {
+          profileName: localUser.name,
+          profileEmail: localUser.email,
+        } }
+        onSubmit={ async (values) => {
+          const result = await userUpdate(localUser.id, values.profileName);
+          /*  if (result.status !== statusCode) {
+                setError(result.data.message);
+                setIsOpen(true);
+                formik.resetForm();
+                return null;
+              } */
+          localStorage.user = JSON.stringify(result);
+          setAlertMsg('Atualização concluída com sucesso');
+          setIsOpen(true);
+        } }
+      >
+        {({
+          isSubmitting, handleSubmit, handleChange, handleBlur, values, errors,
+        }) => (
+          <form onSubmit={ handleSubmit }>
+            <input
+              type="text"
+              onChange={ (e) => { handleChange(e); setHasChanged(false); } }
+              onBlur={ handleBlur }
+              value={ values.profileName }
+              data-testid="profile-name"
+              name="profileName"
+            />
+            <br />
+            <input
+              type="text"
+              value={ values.profileEmail }
+              data-testid="profile-email"
+              disabled
+              name="profileEmail"
+            />
+            <br />
+            {errors.name && <div id="feedback">{errors.name}</div>}
+            <Button type="submit" disabled={ hasChanged || isSubmitting }>Submit</Button>
+          </form>
+        )}
+      </Formik>
+    </>
   );
 };
 export default Profile;
