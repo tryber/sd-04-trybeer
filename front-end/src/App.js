857bdca94264db5384e8098a0f2e6e01c2ce3f32
wistyk <leonardoxw@gmail.com>
2020-11-24 14:25:01 -0300

    register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Login from './components/Login';
-// import UserRegister from './components/UserRegister';
+import UserRegister from './components/UserRegister';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      {/* <Route path="/register" component={ UserRegister } /> */}
+      <Route path="/register" component={ UserRegister } />
       <Route path="/login" component={ Login } />
       <Route exact path="/" component={ Login } />
     </Switch>
