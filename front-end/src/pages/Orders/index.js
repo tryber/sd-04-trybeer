296c81614a312f9241f4874fc4f062022e47a073
Josias Viveiro <josiasviveiro@gmail.com>
2020-11-24 15:03:48 -0300

    Data-testid dos elementos de Orders

--- front-end/src/pages/Orders/index.js
+++ front-end/src/pages/Orders/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import MenuAdmin from '../../components/MenuAdmin';
 
+// Requisito 7 - Criar Tela de Meus Pedidos
 const Orders = () => (
-  <MenuAdmin />
+  <div>
+    <MenuAdmin />
+    <p data-testid="top-title">Meus Pedidos</p>
+    <div data-testid="0-order-card-container">
+      <p data-testid="0-order-number"> Pedido 000</p>
+      <p data-testid="0-order-date">Dia/Mês</p>
+      <p data-testid="0-order-total-value">R$ 0,00</p>
+    </div>
+  </div>
 );
 export default Orders;
