6c9d973c6d88668ea65b51e4ce1de1f1e0956217
wistyk <leonardoxw@gmail.com>
2020-11-24 11:31:35 -0300

    CC

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -12,67 +12,74 @@ const Login = () => {
 
   const [password, setPassword] = useState('');
 
+  const six = 6;
   //  VALIDAÇÃO DE EMAIL
   const isEmailValid = (email = '') => email.match(/\S+@\w+\.\w{2,6}(\.\w{2})?/i);
 
   const emailValidated = () => {
-    if (!eMail || !isEmailValid(eMail) || eMail.length < 6) return false;
+    if (!eMail || !isEmailValid(eMail) || eMail.length < six) return false;
     return true;
   };
 
   // VARIÁVEL QUE DEFINE O "ESTADO" DO BOTÃO
-  const disableButton = !password || password.length < 6 || !emailValidated();
+  const disableButton = !password || password.length < six || !emailValidated();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const api = API.loginApi(eMail, password)
-    .then((api) => {
+    API.loginApi(eMail, password)
+      .then((api) => {
+        const { name, email, role } = api.data.user;
+        const { token } = api.data;
 
-    const { name, email, role } = api.data.user
-    const token = api.data.token;
-    
-    localStorage.setItem('user', JSON.stringify({ name, email, role, token }));
-    
-    setErrorMsg('');
-    
-    if (api.data.user.role === 'client') return history.push('/products');
-    
-    return history.push('/admin/orders');
-  })
-  .catch((error) => setErrorMsg(error))
-    
+        localStorage.setItem('user', JSON.stringify({
+          name, email, role, token,
+        }));
+
+        setErrorMsg('');
+
+        if (api.data.user.role === 'client') return history.push('/products');
+
+        return history.push('/admin/orders');
+      })
+      .catch((error) => setErrorMsg(error));
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="m-3">
+      <form onSubmit={ handleSubmit } className="m-3">
         <section className="card w-75 mx-auto m-3">
           <div className="form-group w-75 mx-auto m-2">
-            <label htmlFor="email">Email</label>
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              data-testid="email-input"
-              type="text"
-              name="email"
-              id="email"
-              required
-              className="form-control"
-            />
+            <label htmlFor="email">
+              Email
+              <input
+                onChange={ (e) => setEmail(e.target.value) }
+                data-testid="email-input"
+                type="text"
+                name="email"
+                id="email"
+                required
+                className="form-control"
+              />
+            </label>
+
           </div>
 
           <div className="form-group w-75 mx-auto m-2">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              data-testid="password-input"
-              type="password"
-              name="password"
-              id="password"
-              minLength="6"
-              required
-              className="form-control"
-            />
+            <label htmlFor="password">
+              Password
+              <input
+                onChange={ (e) => setPassword(e.target.value) }
+                data-testid="password-input"
+                type="password"
+                name="password"
+                id="password"
+                minLength="6"
+                required
+                className="form-control"
+              />
+            </label>
+
           </div>
 
           <span className="mx-auto m-3 text-danger">{errorMsg.toUpperCase()}</span>
@@ -81,7 +88,7 @@ const Login = () => {
             <button
               data-testid="signin-btn"
               type="submit"
-              disabled={disableButton}
+              disabled={ disableButton }
               className="btn btn-warning m-2"
             >
               ENTRAR
