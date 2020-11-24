6c60f5c54980ea6c9b93a5894b854fd30511e25c
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-11-24 14:39:31 -0300

    agora vai

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import 'rbx/index.css';
+
 import { Container } from 'rbx';
 
 import Provider from './context/provider';
