fb8185ff47edd1b478530897cfcafad7689c4ea4
wistyk <leonardoxw@gmail.com>
2020-11-24 11:11:13 -0300

    CC

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Login from './components/Login';
 import UserRegister from './components/UserRegister';
-import UserProfile from './components/UserProfile/index';
 
 const App = () => (
   <BrowserRouter>
@@ -10,7 +9,6 @@ const App = () => (
       <Route path="/register" component={ UserRegister } />
       <Route path="/login" component={ Login } />
       <Route exact path="/" component={ Login } />
-      <Route path="/profile" component={ UserProfile } />
     </Switch>
   </BrowserRouter>
 );
