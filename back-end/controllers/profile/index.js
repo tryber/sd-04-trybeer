b84e3a42ccbd9c2d28a8fce33ede74651e583cd2
Mateus-Moreira <ashuraw@hotmail.com>
2020-11-24 13:46:21 -0300

    adicionada a sugestão do pedro no profile

--- back-end/controllers/profile/index.js
+++ back-end/controllers/profile/index.js
@@ -1,10 +1,14 @@
-const { updateUser } = require('../../models/users');
+const { updateUser, getById } = require('../../models/users');
 
 const userUpdate = async (req, res) => {
   const { id, name } = req.body;
   try {
     await updateUser(id, name);
-    res.status(200).json({ message: 'Usuário atualizado com sucesso' });
+    const user = getById(id);
+    const { password: _, ...userWithoutPassword } = user;
+    const token = createToken(userWithoutPassword);
+
+    res.status(200).json(token);
   } catch {
     res.status(500).json({ message: 'Falha ao atualizar o usuário' });
   }
