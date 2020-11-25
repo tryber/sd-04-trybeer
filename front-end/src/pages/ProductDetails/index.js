3a589263728e55d045d968b125812a01d82984c2
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-25 20:44:59 -0300

    Adicionado Header na tela de detalhes do produto

--- front-end/src/pages/ProductDetails/index.js
+++ front-end/src/pages/ProductDetails/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 import ProductDetailCard from '../../components/ProductDetailsCard';
 
@@ -6,20 +7,22 @@ import './styles.css';
 
 const ProductDetails = () => (
   <div className="product-details-container">
-    <div>Aqui vai um header...</div>
-    <div className="order-detail-info">
+    <Header title="Detalhes de Pedido" />
+    <div className="order-details-card-container">
+      <div className="order-detail-info">
+        <p>
+          Pedido&nbsp;
+          <span data-testid="order-number">0001</span>
+          <span data-testid="order-date">26/10</span>
+        </p>
+      </div>
+      {/* Rodar map() depois */}
+      <ProductDetailCard testid={ 1 } />
       <p>
-        Pedido&nbsp;
-        <span data-testid="order-number">0001</span>
-        <span data-testid="order-date">26/10</span>
+        Total: &nbsp;
+        <span data-testid="order-total-value">2,20</span>
       </p>
     </div>
-    {/* Rodar map() depois */}
-    <ProductDetailCard testid={ 1 } />
-    <p>
-      Total: &nbsp;
-      <span data-testid="order-total-value">2,20</span>
-    </p>
   </div>
 );
 
