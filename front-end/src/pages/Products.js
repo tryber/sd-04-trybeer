4e0f95934e06df4d89909f638fa7fb320ef1f3d4
henrique <hbaetaleite@gmail.com>
2020-11-25 16:06:06 -0300

    cc

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -32,13 +32,17 @@ const Products = () => {
   if (!userIsLogged) return <Redirect to="/login" />;
 
   const addProduct = async (product) => {
-    const { id, name, urlImage, price, quantity } = product;
+    const {
+      id, name, urlImage, price, quantity,
+    } = product;
 
     const cartWithoutNewProduct = productCart.filter((prodCart) => prodCart.id !== product.id);
 
     const newAmount = quantity ? parseInt(quantity) + 1 : 1;
 
-    const newProduct = { id, name, urlImage, price, quantity: newAmount };
+    const newProduct = {
+      id, name, urlImage, price, quantity: newAmount,
+    };
 
     console.log('novo item comprado', newProduct);
     console.log('carrinho sem o produto atual', cartWithoutNewProduct);
@@ -65,36 +69,36 @@ const Products = () => {
       <h3 data-testid="top-title">Página de produtos</h3>
       <div className="container d-flex flex-wrap">
         {products.map((product, index) => (
-          <div className="card w-50 mx-auto m-3" key={product.id}>
-            <h4 data-testid={`${index}-product-name`} className="card-title text-center">
+          <div className="card w-50 mx-auto m-3" key={ product.id }>
+            <h4 data-testid={ `${index}-product-name` } className="card-title text-center">
               {product.name}
             </h4>
-            <h5 data-testid={`${index}-product-price`} className="card-content mx-auto">
+            <h5 data-testid={ `${index}-product-price` } className="card-content mx-auto">
               {product.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
             </h5>
             <img
               className="mx-auto m-2"
               height="90px"
-              data-testid={`${index}-product-img`}
-              alt={product.name}
-              src={product.urlImage}
+              data-testid={ `${index}-product-img` }
+              alt={ product.name }
+              src={ product.urlImage }
             />
             <section className="mx-auto m-1 d-flex align-items-center justify-content-center">
               <button
-                data-testid={`${index}-product-plus`}
-                onClick={() => addProduct(product)}
+                data-testid={ `${index}-product-plus` }
+                onClick={ () => addProduct(product) }
                 className="btn btn-success"
                 type="button"
               >
                 +
               </button>
-              <h1 data-testid={`${index}-product-qtd`} className="card m-2">
+              <h1 data-testid={ `${index}-product-qtd` } className="card m-2">
                 {product.quantity || 0}
               </h1>
               <button
-                data-testid={`${index}-product-minus`}
-                onClick={() => removeProduct(product)}
-                disabled={buttonDisabled}
+                data-testid={ `${index}-product-minus` }
+                onClick={ () => removeProduct(product) }
+                disabled={ buttonDisabled }
                 className="btn btn-danger"
                 type="button"
               >
@@ -109,7 +113,8 @@ const Products = () => {
         className="fixed-bottom btn btn-info mx-auto w-75 m-2"
         to="/checkout"
       >
-        Ver Carrinho{' '}
+        Ver Carrinho
+        {' '}
         {totalValue === 0
           ? ''
           : totalValue.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
