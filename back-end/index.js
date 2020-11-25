b5956cecc4885d48a6543de40f91c6c29d61291f
danilossousa <danilosantossousa@gmail.com>
2020-11-25 16:35:22 -0300

    correçao 8

--- back-end/index.js
+++ back-end/index.js
@@ -4,13 +4,14 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
+const port = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/', (_, res) => res.send('ok'));
+// app.get('/', (_, res) => res.send('ok'));
 
 app.use('/images', express.static('images'));
 
