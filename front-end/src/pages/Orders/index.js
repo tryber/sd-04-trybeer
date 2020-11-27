a897267462d82bc235ac6e8c136229939ae008c8
Josias Viveiro <josiasviveiro@gmail.com>
2020-11-27 18:13:03 -0300

    Lista os pedidos na pág. de Cliente - Meus Pedidos

--- front-end/src/pages/Orders/index.js
+++ front-end/src/pages/Orders/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Flex,
   Text,
 } from '@chakra-ui/react';
 import MenuClient from '../../components/MenuClient';
 import OrderCard from '../../components/OrderCard';
+import { getOrders } from '../../api';
 
 // Requisito 7 - Criar Tela de Meus Pedidos
 /*
@@ -12,17 +13,49 @@ Banco de Dados
 table: sales
 columns:
   id, user_id, total_price, sale_date, delivery_number
-*/
-// 1 - top-title -> alterar no MenuClient/Header parar receber uma props.title
-// 2 - fazer o mock do Order para testar a tela
 
-const Orders = () => (
-  <div>
-    <MenuClient />
-    <Text data-testid="top-title">Meus Pedidos</Text>
-    <Flex direction="column">
-      <OrderCard />
-    </Flex>
-  </div>
-);
+A busca no banco de dados precisa ser feita pelo user_id  
+*/
+// 1 - top-title -> alterar no MenuClient/Header
+// para ter um context para o title
+
+// - Pegar o id do usuário (user_id) para acessar seus pedidos
+//   (salesByUserId? no BACKEND)
+
+/*
+  - Ao entrar na tela, se o usuário não estiver logado, deve ser redirecionado para a tela Login.
+    1. Verificar se tem usuário no Local Storage?
+*/
+
+const Orders = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    // Pegando os pedidos do banco de dados
+    getOrders()
+      .then((response) => {
+        console.log("Response: ", response);
+        setOrders(response.data);
+      })
+      .catch(() => 'um erro ocorreu');
+    
+    }, []);
+    console.log("Orders: ", orders);
+
+  return (
+    <div>
+      <MenuClient />
+      <Text data-testid="top-title">Meus Pedidos</Text>
+      <Flex direction="column">
+        {orders ? orders.map((e) =>
+          <OrderCard
+            data={e}
+            key={e.id}
+          />
+        ) : <Text>Loading...</Text>}
+      </Flex>
+    </div>
+  );
+};
+
 export default Orders;
