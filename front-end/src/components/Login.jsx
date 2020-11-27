a329d10dec59354f03fa1f5957b26c78feb510f3
wistyk <leonardoxw@gmail.com>
2020-11-26 23:35:08 -0300

    CC login

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -15,86 +15,95 @@ const Login = () => {
   const [password, setPassword] = useState('');
 
   //  VALIDAÇÃO DE EMAIL
-  const isEmailValid = (email = '') => email.match(/\S+@\w+\.\w{2,6}(\.\w{2})?/i);
+  const isEmailValid = (email = '') =>
+    email.match(/\S+@\w+\.\w{2,6}(\.\w{2})?/i);
+
+  const six = 6;
 
   const emailValidated = () => {
-    if (!eMail || !isEmailValid(eMail) || eMail.length < 6) return false;
+    if (!eMail || !isEmailValid(eMail) || eMail.length < six) return false;
     return true;
   };
 
   // VARIÁVEL QUE DEFINE O "ESTADO" DO BOTÃO
-  const disableButton = !password || password.length < 6 || !emailValidated();
+  const disableButton = !password || password.length < six || !emailValidated();
 
   const handleSubmit = () => {
     // e.preventDefault();
 
     API.loginApi(eMail, password)
 
-    .then((api) => {
+      .then((api) => {
+        // if (api.data.user.err) return setErrorMsg(api.data.user.err);
 
-    // if (api.data.user.err) return setErrorMsg(api.data.user.err);
+        const { name, email, role } = api.data.user;
+        const { token } = api.data;
 
-    const { name, email, role } = api.data.user;
-    const token = api.data.token;
+        localStorage.setItem(
+          'user',
+          JSON.stringify({ name, email, role, token })
+        );
 
-    localStorage.setItem('user', JSON.stringify({ name, email, role, token }));
+        setErrorMsg('');
 
-    setErrorMsg('');
+        console.log(api);
+        if (api.data.user.role === 'client') return history.push('/products');
 
-    console.log(api)
-    if (api.data.user.role === 'client') return history.push('/products');
-
-    return history.push('/admin/orders');
-    })
-    .catch((error) => setErrorMsg(error.response.data));
+        return history.push('/admin/orders');
+      })
+      .catch((error) => setErrorMsg(error.response.data));
   };
 
   return (
     <div>
       <Header title='TryBeer' />
-      <form className="m-3">
-        <section className="card w-75 mx-auto m-3">
-          <div className="form-group w-75 mx-auto m-2">
-            <label htmlFor="email">Email</label>
+      <form className='m-3'>
+        <section className='card w-75 mx-auto m-3'>
+          <div className='form-group w-75 mx-auto m-2'>
+            <label htmlFor='email'>
+              Email{ ' ' }
+              <input
+                onChange={ (e) => setEmail(e.target.value) }
+                data-testid='email-input'
+                type='text'
+                name='email'
+                id='email'
+                required
+                className='form-control'
+              />
+            </label>
+          </div>
+
+          <div className='form-group w-75 mx-auto m-2'>
+            <label htmlFor='password'>Password</label>
             <input
-              onChange={(e) => setEmail(e.target.value)}
-              data-testid="email-input"
-              type="text"
-              name="email"
-              id="email"
+              onChange={ (e) => setPassword(e.target.value) }
+              data-testid='password-input'
+              type='password'
+              name='password'
+              id='password'
+              minLength='6'
               required
-              className="form-control"
+              className='form-control'
             />
           </div>
 
-          <div className="form-group w-75 mx-auto m-2">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              data-testid="password-input"
-              type="password"
-              name="password"
-              id="password"
-              minLength="6"
-              required
-              className="form-control"
-            />
-          </div>
+          <span className='mx-auto m-3 text-danger'>
+            { errorMsg.toUpperCase() }
+          </span>
 
-          <span className="mx-auto m-3 text-danger">{errorMsg.toUpperCase()}</span>
-
-          <div className="mx-auto m-2">
+          <div className='mx-auto m-2'>
             <button
-              data-testid="signin-btn"
-              type="button"
-              disabled={disableButton}
-              className="btn btn-warning m-2"
-              onClick={() => handleSubmit()}
+              data-testid='signin-btn'
+              type='button'
+              disabled={ disableButton }
+              className='btn btn-warning m-2'
+              onClick={ () => handleSubmit() }
             >
               ENTRAR
             </button>
 
-            <Link data-testid="no-account-btn" to="/register" className="m-2">
+            <Link data-testid='no-account-btn' to='/register' className='m-2'>
               Ainda não tenho conta
             </Link>
           </div>
