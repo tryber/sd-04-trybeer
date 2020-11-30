5071e879efe6ac977587f5d90a7ecce3f2454427
wistyk <leonardoxw@gmail.com>
2020-11-30 19:12:49 -0300

    tela montada, pendente configuraçao das açoes

--- front-end/src/components/Products/index.js
+++ front-end/src/components/Products/index.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import API from '../../services/api';
 
 import Header from '../Header';
 import SideBar from '../SideBarCLI';
 
-const Products = () => (
-  <div>
-    <Header title="TryBeer" />
-    <SideBar />
-  </div>
-);
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('user')).token;
+    API.getProducts(token).then((products) => setProducts(products.data));
+  }, []);
+  return (
+    <div>
+      <Header title='Cliente - Produtos' />
+      <SideBar />
+      {products.map((item) => (
+        <div key={item.id} className='card' style={{ width: '8rem' }}>
+          <img src={item.urlImg} className='card-img-top' alt={item.name} />
+          <div className='card-body'>
+            <h5 className='card-title'>{item.name}</h5>
+            <p className='card-text'>
+              {item.price.toLocaleString('pt-br', {
+                style: 'currency',
+                currency: 'BRL',
+              })}
+            </p>
+            <button type='button' className='btn btn-primary'>
+              +
+            </button>
+            <button type='button' className='btn btn-danger'>
+              -
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Products;
