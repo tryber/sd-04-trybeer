df297da40883f1d57fa6a94bfc5066e3a2606733
wistyk <leonardoxw@gmail.com>
2020-11-30 19:21:10 -0300

    CC

--- front-end/src/components/Products/index.js
+++ front-end/src/components/Products/index.js
@@ -8,28 +8,28 @@ import SideBar from '../SideBarCLI';
 const Products = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    const token = JSON.parse(localStorage.getItem('user')).token;
-    API.getProducts(token).then((products) => setProducts(products.data));
+    const { token } = JSON.parse(localStorage.getItem('user'));
+    API.getProducts(token).then((items) => setProducts(items.data));
   }, []);
   return (
     <div>
-      <Header title='Cliente - Produtos' />
+      <Header title="Cliente - Produtos" />
       <SideBar />
       {products.map((item) => (
-        <div key={item.id} className='card' style={{ width: '8rem' }}>
-          <img src={item.urlImg} className='card-img-top' alt={item.name} />
-          <div className='card-body'>
-            <h5 className='card-title'>{item.name}</h5>
-            <p className='card-text'>
+        <div key={ item.id } className="card" style={ { width: "8rem" } }>
+          <img src={ item.urlImg } className="card-img-top" alt={ item.name } />
+          <div className="card-body">
+            <h5 className="card-title">{item.name}</h5>
+            <p className="card-text">
               {item.price.toLocaleString('pt-br', {
                 style: 'currency',
                 currency: 'BRL',
               })}
             </p>
-            <button type='button' className='btn btn-primary'>
+            <button type="button" className="btn btn-primary">
               +
             </button>
-            <button type='button' className='btn btn-danger'>
+            <button type="button" className="btn btn-danger">
               -
             </button>
           </div>
