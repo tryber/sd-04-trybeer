f83af43863e4bdc433eb63ee3d6131d854fc39f2
Mateus-Moreira <ashuraw@hotmail.com>
2020-12-01 17:41:44 -0300

    update

--- back-end/models/sales/index.js
+++ back-end/models/sales/index.js
@@ -2,18 +2,16 @@ const connection = require('../dbConnection/index');
 
 const findOrderByUserId = async (UId) => {
   const userOrder = await connection()
-    .then((db) =>
-      db .getTable('sales') .select([]) .where('user_id = :userId') .bind('userId', UId)
-        .execute()
-    )
+    .then((db) => db.getTable('sales').select([])
+      .where('user_id = :userId')
+      .bind('userId', UId)
+        .execute(),)
     .then((results) => results.fetchAll())
-    .then((sales) =>
-      sales.map(
-        ([ id, userId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status, ]) => ({ 
+    .then((sales) => sales.map(
+        ([id, userId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status,]) => ({
           id, userId, totalPrice, deliveryAddress, deliveryNumber, saleDate, status,
-        })
-      )
-    );
+        }),
+      ),);
 
   return userOrder;
 };
