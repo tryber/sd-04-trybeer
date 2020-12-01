2692744e5e933cb24d07821ef0782576709cea66
wistyk <leonardoxw@gmail.com>
2020-12-01 13:27:15 -0300

    correcao na model

--- back-end/models/productsModel.js
+++ back-end/models/productsModel.js
@@ -4,7 +4,7 @@ const read = async () => {
   const table = await conn().then((db) => db.getTable('products'));
   const products = await table.select([]).execute();
 
-  return products;
+  return products.fetchAll().map(([id, name, price, urlImg]) => ({ id, name, price, urlImg }));;
 };
 
 // IIFE para testes, ignorem!!!
