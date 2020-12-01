c97af03a340872c24595ba545365b53257ad75c9
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-30 22:00:42 -0300

    Usando dicas do Renatão

--- back-end/services/productsService.js
+++ back-end/services/productsService.js
@@ -6,7 +6,12 @@ const listSales = async () => productsModel.getAllSales();
 
 const newSale = async (payload) => productsModel.insertNewSale(payload);
 
-const getSaleById = async (saleId) => productsModel.getSaleById(saleId);
+const getSaleById = async (saleId) => {
+  const { date: oldDate, ...restOfSale } = await productsModel.getSaleById(saleId);
+  const date = `${oldDate.getDate() + 1}/${oldDate.getMonth() + 1}`;
+  return { ...restOfSale, date };
+  // return await productsModel.getSaleById(saleId);
+};
 
 const getSaleProducts = async (id) => productsModel.getSaleProducts(id);
 
