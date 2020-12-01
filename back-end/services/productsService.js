aad1321d9fc7bc7008080b7611c7f83d9163030f
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-30 22:41:01 -0300

    Funciona essa drogaaaaa

--- back-end/services/productsService.js
+++ back-end/services/productsService.js
@@ -8,7 +8,9 @@ const newSale = async (payload) => productsModel.insertNewSale(payload);
 
 const getSaleById = async (saleId) => {
   const { date: oldDate, ...restOfSale } = await productsModel.getSaleById(saleId);
-  const date = `${oldDate.getDate() + 1}/${oldDate.getMonth() + 1}`;
+  const settedDate = new Date(new Date().setDate(oldDate.getDate() + 1));
+  // console.log(settedDate.getUTCMonth());
+  const date = `${settedDate.getUTCDate()}/${settedDate.getUTCMonth() + 1}`;
   return { ...restOfSale, date };
   // return await productsModel.getSaleById(saleId);
 };
