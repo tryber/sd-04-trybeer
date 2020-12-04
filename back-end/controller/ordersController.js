ec4867c4403a8f9303512c5553ab5ca25eccacca
wistyk <leonardoxw@gmail.com>
2020-12-04 12:28:12 -0300

    CC

--- back-end/controller/ordersController.js
+++ back-end/controller/ordersController.js
@@ -1,11 +1,9 @@
 const ordersModel = require('../models/ordersModel');
 
 const setOrder = async (req, res) => {
-
-  const { id, total, num, street, date } = req.body
+  const { id, total, num, street, date } = req.body;
   await ordersModel.setOrder(id, total, num, street, date);
-  const orders = await ordersModel.getOrder();
-  res.status(200).json( 'Ordered' );
+  res.status(200).json('Ordered');
 };
 
 module.exports = {
--- back-end/models/ordersModel.js
+++ back-end/models/ordersModel.js
@@ -4,16 +4,43 @@ const getOrder = async () => {
   const table = await conn().then((db) => db.getTable('sales'));
   const sales = await table.select([]).execute();
 
-  return sales.fetchAll().map(([id, user_id, total_price, delivery_address, delivery_number, sale_date, status]) => ({ id, user_id, total_price, delivery_address, delivery_number, sale_date, status }));
+  return sales
+    .fetchAll()
+    .map(
+      ([
+        id,
+        user_id,
+        total_price,
+        delivery_address,
+        delivery_number,
+        sale_date,
+        status,
+      ]) => ({
+        id,
+        user_id,
+        total_price,
+        delivery_address,
+        delivery_number,
+        sale_date,
+        status,
+      })
+    );
 };
 
 const setOrder = async (id, total, num, street, date) =>
   conn().then((db) =>
     db
       .getTable('sales')
-      .insert(['user_id', 'total_price', 'delivery_address', 'delivery_number', 'sale_date', 'status'])
+      .insert([
+        'user_id',
+        'total_price',
+        'delivery_address',
+        'delivery_number',
+        'sale_date',
+        'status',
+      ])
       .values(id, total, street, num, date, 'sold')
-      .execute(),
+      .execute()
   );
 
 module.exports = {
