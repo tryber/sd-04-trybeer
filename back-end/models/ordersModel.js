502fcd6e24db5759feac7a0d7445b5058e5bb477
wistyk <leonardoxw@gmail.com>
2020-12-04 15:07:49 -0300

    teste dos parenteses

--- back-end/models/ordersModel.js
+++ back-end/models/ordersModel.js
@@ -8,14 +8,15 @@ const getOrders = async () => {
     .fetchAll();
 };
 
-const setOrder = async (id, total, num, street, date) =>
+const setOrder = async (id, total, num, street, date) => (
   connection()
     .then((db) => db
       .getTable('sales')
       .insert(['user_id', 'total_price', 'delivery_address', 'delivery_number', 'sale_date', 'status',
-    ])
+      ])
       .values(id, total, street, num, date, 'sold')
       .execute());
+);
 
 module.exports = {
   getOrders,
