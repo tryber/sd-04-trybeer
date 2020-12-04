04f82d3468cb0cc7668ee0fbeff9f7218c7146dd
Josias Viveiro <josiasviveiro@gmail.com>
2020-12-04 12:02:25 -0300

    Controller Checkout funcionando

--- back-end/controllers/checkout/index.js
+++ back-end/controllers/checkout/index.js
@@ -4,8 +4,13 @@ const checkout = async (req, res) => {
   const { products, status, date, userId, cartValue, addressValue, numberValue } = req.body;
   try {
     const resultSale = await addSale(userId, cartValue, addressValue, numberValue, date, status);
-    const { id } = resultSale;
-    const resultSaleProd = products.forEach(async (e) => addSaleProduct(id, e.id, e.quantity));
+
+    const resultSaleProd = await Promise.all(
+      products.map(async (e) => {
+        return await addSaleProduct(resultSale, e.id, e.quantity);
+      })
+    );
+
     return res.status(200).json({ resultSale, resultSaleProd });
   } catch (error) {
     return res.status(500).json({ message: 'I have bad news' });
