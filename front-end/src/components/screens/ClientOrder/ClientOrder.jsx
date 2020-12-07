ed5ce173e7e0cacddcb241fe6f040f9b9838272f
igorgbr <igorgphotoarte@gmail.com>
2020-12-07 20:01:22 -0300

    cc do

--- front-end/src/components/screens/ClientOrder/ClientOrder.jsx
+++ front-end/src/components/screens/ClientOrder/ClientOrder.jsx
@@ -32,20 +32,18 @@ const ClientOrder = () => {
           (newDate = new Date(e.saleDate)),
           (day = Intl.DateTimeFormat('en', { day: '2-digit' }).format(newDate)),
           (month = Intl.DateTimeFormat('en', { month: '2-digit' }).format(
-            newDate
+            newDate,
           )),
           (
             <div key={ e.idSale } data-testid="order-card-container">
               <a href={ `/orders/${e.idSale}` }>
                 <p data-testid={ `${i}-order-number` }>{ `Pedido ${e.idSale}` }</p>
                 <p data-testid={ `${i}-order-date` }>{ `${day}/${month}` }</p>
-                <p
-                  data-testid={ `${i}-order-total-value` }
-                >{ `R$ ${e.totalPrice}` }</p>
+                <p data-testid={ `${i}-order-total-value` }>{ `R$ ${e.totalPrice}` }</p>
               </a>
             </div>
           )
-        )
+        ),
       )}
     </>
   );
