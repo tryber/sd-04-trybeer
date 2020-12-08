9f3dbb1cf730cf419baffb1a41642d29a45876d3
igorgbr <igorgphotoarte@gmail.com>
2020-12-07 21:22:36 -0300

    teste cc de novo

--- front-end/src/components/screens/ClientOrder/ClientOrder.jsx
+++ front-end/src/components/screens/ClientOrder/ClientOrder.jsx
@@ -21,29 +21,29 @@ const ClientOrder = () => {
     })();
   }, []);
 
-  let newDate = '';
   let day = '';
   let month = '';
   return (
     <>
       <Header title="Cliente - Meus Pedidos" />
       {pedidos.map(
-        (e, i) => (
-          (newDate = new Date(e.saleDate)),
-          (day = Intl.DateTimeFormat('en', { day: '2-digit' }).format(newDate)),
+       ((e, i) => (
+          (day = Intl.DateTimeFormat('en', { day: '2-digit' }).format(
+            new Date(e.saleDate)
+          )),
           (month = Intl.DateTimeFormat('en', { month: '2-digit' }).format(
-            newDate
+            new Date(e.saleDate),
           )),
           (
-            <div key={ e.idSale} data-testid="order-card-container">
+            <div key={ e.idSale } data-testid="order-card-container">
               <a href={ `/orders/${e.idSale}` }>
                 <p data-testid={ `${i}-order-number` }>{ `Pedido ${e.idSale}` }</p>
                 <p data-testid={ `${i}-order-date` }>{ `${day}/${month}` }</p>
-                <p data-testid={`${i}-order-total-value` }>{ `R$ ${e.totalPrice}` }</p>
+                <p data-testid={ `${i}-order-total-value` }>{ `R$ ${e.totalPrice}` }</p>
               </a>
             </div>
           )
-        )
+        )),
       )}
     </>
   );
