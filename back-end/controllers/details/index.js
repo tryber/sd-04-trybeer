b8c50b5aae9036621bd59d392baf64ad2156d2d8
Ronald Lima <ronaldazevedolima@gmail.com>
2020-12-09 15:31:51 -0300

    req8

--- back-end/controllers/details/index.js
+++ back-end/controllers/details/index.js
@@ -1,6 +1,14 @@
-const { getDetail } = require('../../models/details');
-const {}
+const { getDetail, getSaleInfo } = require('../../models/details');
 
 const getDetailController = async (req, res) => {
-  const saleInf = await
+  try {
+    const saleInf = await getSaleInfo();
+    const prodInfo = await getDetail();
+    return res.status(200).json({saleInf, prodInfo})
+    
+  } catch (error) {
+    return res.status(500).json({ message: 'I have bad news' });
+  }
 }
+
+module.exports = getDetailController
